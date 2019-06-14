023aaedcf9e7d2790bcb27c2637b13d318386852
Kirill Olokin <kirill.olokin@stud.tu-darmstadt.de>
2019-06-14 03:20:40 +0200

    pipeline almost done TODO inherited dependencies

--- PETGraph.py
+++ PETGraph.py
@@ -1,10 +1,13 @@
 import numpy as np
+from graph_tool import Graph, GraphView
 from graph_tool.all import Graph
-from graph_tool.all import graph_draw, radial_tree_layout, minimize_nested_blockmodel_dl, draw_hierarchy, GraphView
+from graph_tool.all import graph_draw, minimize_nested_blockmodel_dl, draw_hierarchy, GraphView, graphviz_draw, \
+    interactive_window
 from graph_tool.search import bfs_iterator, dfs_iterator
+from graph_tool.draw import sfdp_layout, fruchterman_reingold_layout, arf_layout, planar_layout, random_layout, \
+    radial_tree_layout
 
 from parser import readlineToCUIdMap, writelineToCUIdMap, lineToCUIdMap
-
 
 node_type_info = {
     '0': {
@@ -50,6 +53,10 @@ GT_map_node_indices = dict()
 
 
 class PETGraph(object):
+    children_graph: GraphView
+    dep_graph: GraphView
+    graph: Graph
+
     def __init__(self, cu_dict, dependences_list):
         self.graph = Graph()
 
@@ -75,13 +82,13 @@ class PETGraph(object):
                     sink = self.graph.vertex(GT_map_node_indices[child])
                     e = self.graph.add_edge(source, sink)
                     self.graph.ep.type[e] = 'child'
-                    self.graph.ep.viz_color[e] = [0.179, 0.203 ,0.210, 0.8]
+                    self.graph.ep.viz_color[e] = [0.0, 0.0, 0.5, 1]
             if 'successors' in dir(node) and 'CU' in dir(node.successors):
                 for successor in node.successors.CU:
                     sink = self.graph.vertex(GT_map_node_indices[successor])
                     e = self.graph.add_edge(source, sink)
                     self.graph.ep.type[e] = 'successor'
-                    self.graph.ep.viz_color[e] = [0.579, 0.503 ,0.010, 0.8]
+                    self.graph.ep.viz_color[e] = [0.0, 0.5, 0.0, 0.5]
 
         for dep in dependences_list:
             sink_cu_ids = lineToCUIdMap[dep.sink] if dep.type == 'INIT' else readlineToCUIdMap[dep.sink]
@@ -98,8 +105,8 @@ class PETGraph(object):
                             sink_v = self.graph.vertex(GT_map_node_indices[sink_cu_id])
                             e = self.graph.add_edge(sink_v, source_v)
                             self.graph.ep.type[e] = 'dependence'
-                            self.graph.ep.viz_dash_style[e] = [.01, .01, 0]
-                            self.graph.ep.viz_color[e] = [0.279, 0.203, 0.210, 0.8]
+                            self.graph.ep.viz_dash_style[e] = [1, 1, 0]
+                            self.graph.ep.viz_color[e] = [0.4, 0.4, 0.4, 0.5]
 
         self.dep_graph = self.filter_view(edges_type='dependence')
         self.children_graph = self.filter_view(edges_type='child')
@@ -115,20 +122,42 @@ class PETGraph(object):
         root_idx = in_degrees.tolist().index(0)
         return self.graph.vertex(root_idx)
 
-    def infer_level_dependences(self):
+    def infer_level_dependencies(self):
         for level in bfs_iterator(self.children_graph, source=self.get_root_node()):
             print(self.graph.vp.id[level.source()], self.graph.vp.id[level.target()])
             # for depth in dfs_iterator()
 
+    def interactive_visualize(self, view=None):
+        view = view if view else self.graph
+        interactive_window(view,
+                           vprops={'text': self.graph.vp.id,
+                                   'fill_color': self.graph.vp.viz_color,
+                                   'shape': self.graph.vp.viz_shape},
+                           eprops={'color': self.graph.ep.viz_color})
 
-    def visualize(self, view=None):
+    def visualize(self, view=None, filename='output.svg'):
         view = view if view else self.graph
 
-        pos = radial_tree_layout(view, self.get_root_node())
-        graph_draw(view, pos=pos, vertex_shape=self.graph.vp.viz_shape,
+        # layout = radial_tree_layout(view, self.get_root_node())
+        # layout = sfdp_layout(view)
+        # layout = fruchterman_reingold_layout(view)
+        layout = arf_layout(view)  # ok
+        # layout = planar_layout(view) # SIGSEGV
+        # layout = random_layout(view) # no
+        graph_draw(view, pos=layout, vertex_shape=self.graph.vp.viz_shape,
                    vertex_text=self.graph.vp.id,
                    vertex_fill_color=self.graph.vp.viz_color,
-                   edge_marker_size=20,
+                   edge_marker_size=10,
                    edge_color=self.graph.ep.viz_color,
                    edge_dash_style=self.graph.ep.viz_dash_style,
-                   vertex_font_size=16, output='output.svg')
+                   vertex_font_size=16, output=filename)
+
+        # SIGSEGV
+        # graphviz_draw(g=view, pos=None, size=(15, 15),
+        #               pin=False, layout='circo',
+        #               maxiter=None, ratio='fill', overlap=False, sep=None,
+        #               splines=False, vsize=0.105, penwidth=1.0, elen=None,
+        #               gprops={}, vprops={'id': view.vp.id}, eprops={},
+        #               vcolor="#a40000", ecolor="#2e3436",
+        #               vcmap=None, vnorm=True, ecmap=None, enorm=True, vorder=None, eorder=None,
+        #               output=filename, output_format="auto", fork=False, return_string=False)
--- pattern_detection.py
+++ pattern_detection.py
@@ -1,46 +1,109 @@
 import numpy as np
 from graph_tool.util import find_edge, find_vertex
 from graph_tool.spectral import adjacency
-from graph_tool.search import dfs_iterator
+from graph_tool.search import dfs_iterator, bfs_iterator
+from graph_tool.all import Vertex, Graph
+from typing import List
+from PETGraph import PETGraph
 
-def find_subNodes(graph, node, criteria):
-    subNodes = []
-    for relevant_edge in graph.get_out_edges(node):
-        if graph.ep.type[graph.edge(relevant_edge[0], relevant_edge[1])] == criteria:
-            subNodes.append(relevant_edge[1])
-    return subNodes
+
+def find_subNodes(graph: Graph, node: Vertex, criteria: str) -> List[Vertex]:
+    return [e.target() for e in node.out_edges() if graph.ep.type[e] == criteria]
+
 
 class PatternDetector(object):
-    def __init__(self, pet_graph):
+    pet: PETGraph
+
+    def __init__(self, pet_graph: PETGraph):
         self.pet = pet_graph
 
-    def __detect_pipeline(self):
-        for loop_node in find_vertex(self.pet.graph, self.pet.graph.vp.type, '2'):
-            loop_subnodes = []
-            for v in find_subNodes(self.pet.graph, loop_node, 'child'):
-                node = self.pet.graph.vertex(v)
-                if self.pet.graph.vp.startsAtLine[node] == self.pet.graph.vp.startsAtLine[loop_node] and self.pet.graph.vp.endsAtLine[node] == self.pet.graph.vp.startsAtLine[loop_node]:
-                    continue
-                if self.pet.graph.vp.startsAtLine[node] == self.pet.graph.vp.endsAtLine[loop_node] and self.pet.graph.vp.endsAtLine[node] == self.pet.graph.vp.endsAtLine[loop_node]:
-                    continue
-                ### TODO: Try to add 3rd check for checking if the startline is in startline of child loops
-                loop_subnodes.append(node)
+    def is_depending(self, v_source: Vertex, v_target: Vertex) -> bool:
+        # TODO RAW dependencies are not inherited (13->11 ...)
+        children = self.get_all_children(v_target)
+        children.append(v_target)
 
-            if len(loop_subnodes) < 2: # No chain of stages found
-                return
+        for e in v_source.out_edges():
+            if self.pet.graph.ep.type[e] == 'dependence' and e.target() in children:
+                return True
+        return False
 
-            # Create graph view for the loop region CUs and then get its adjacency matrix
-            loop_graph = self.pet.filter_view(loop_subnodes, 'dependence')
-            print(adjacency(loop_graph).todense())
-            # self.pet.visualize(loop_graph)
-            # create graph vector
+    def get_all_children(self, root: Vertex) -> List[Vertex]:
+        res = []
+        for e in root.out_edges():
+            if self.pet.graph.ep.type[e] == 'child':
+                if self.pet.graph.vp.type[e.target()] == '0':
+                    res.append(e.target())
+                res.extend(self.get_all_children(e.target()))
+        return res
 
-            # create pipeline pattern vector
+    def is_pipeline_subnode(self, root: Vertex, current: Vertex, children_start_lines: List[str]) -> bool:
+        """checks if node is a valid subnode for pipeline
+        """
+        r_start = self.pet.graph.vp.startsAtLine[root]
+        r_end = self.pet.graph.vp.endsAtLine[root]
+        c_start = self.pet.graph.vp.startsAtLine[current]
+        c_end = self.pet.graph.vp.endsAtLine[current]
+        return not (c_start == r_start and c_end == r_start
+                    or c_start == r_end and c_end == r_end
+                    or c_start == c_end and c_start in children_start_lines)
 
-            #
+    def __detect_pipelines(self):
+        """Search for pipeline pattern
+        """
+        for node in find_vertex(self.pet.graph, self.pet.graph.vp.type, '2'):
+            self.__detect_pipeline(node)
 
+    def __detect_pipeline(self, root: Vertex) -> None:
+        """Calculate pipeline value for node
+        """
+        # TODO faster iteration necessary?
 
+        # TODO how deep
+        children_start_lines = [self.pet.graph.vp.startsAtLine[v]
+                                for v in find_subNodes(self.pet.graph, root, 'child')
+                                if self.pet.graph.vp.type[v] == '2']
+
+        loop_subnodes = [v for v in find_subNodes(self.pet.graph, root, 'child')
+                         if self.is_pipeline_subnode(root, v, children_start_lines)]
+
+        # No chain of stages found
+        if len(loop_subnodes) < 2:
+            self.pet.graph.vp.pipeline[root] = -1
+            return
+
+        # print(adjacency(loop_graph).todense())
+
+        graph_vector = []
+        for i in range(0, len(loop_subnodes) - 1):
+            # Fix: the dependencies of loop-indices can be excluded, if they are read-only in loop body.
+            graph_vector.append(1 if self.is_depending(loop_subnodes[i + 1], loop_subnodes[i]) else 0)
+
+        pipeline_vector = []
+        for i in range(0, len(loop_subnodes) - 1):
+            pipeline_vector.append(1)
+
+        min_weight = 1
+        for i in range(0, len(loop_subnodes) - 1):
+            for j in range(i + 1, len(loop_subnodes)):
+                if self.is_depending(loop_subnodes[i], loop_subnodes[j]):
+                    # TODO whose corresponding entry in the graph matrix is nonzero?
+                    node_weight = 1 - (j - i) / (len(loop_subnodes) - 1)
+                    if min_weight > node_weight > 0:
+                        min_weight = node_weight
+
+        if min_weight == 1:
+            graph_vector.append(0)
+            pipeline_vector.append(0)
+        else:
+            graph_vector.append(1)
+            pipeline_vector.append(min_weight)
+        pipeline_scalar_value = np.dot(graph_vector, pipeline_vector) / (np.linalg.norm(graph_vector)
+                                                                         * np.linalg.norm(pipeline_vector))
+        self.pet.graph.vp.pipeline[root] = pipeline_scalar_value
+        print('Pipeline at ', self.pet.graph.vp.id[root])
+        print('start: ', self.pet.graph.vp.startsAtLine[root])
+        print('end: ', self.pet.graph.vp.endsAtLine[root])
+        print('stages: ', len(loop_subnodes))
 
     def detect_patterns(self):
-        self.__detect_pipeline()
-
+        self.__detect_pipelines()
