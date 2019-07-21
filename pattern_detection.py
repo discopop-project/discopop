85aad0fb4216bfc76feb6dcb0674d580b9ac947c
Kirill Olokin <kirill.olokin@stud.tu-darmstadt.de>
2019-07-21 20:56:03 +0200

    cpp as comments ignore data folder

--- pattern_detection.py
+++ pattern_detection.py
@@ -194,6 +194,124 @@ class PatternDetector(object):
 
         return correlation_coefficient(graph_vector, pattern_vector)
 
+    def __detect_task_parallelism_loop(self):
+        """computes the Task Parallelism Pattern for a node:
+        (Automatic Parallel Pattern Detection in the Algorithm Structure Design Space p.46)
+        1.) first merge all children of the node -> all children nodes get the dependencies
+            of their children nodes and the list of the children nodes (saved in node.childrenNodes)
+        2.) To detect Task Parallelism, we use Breadth First Search (BFS)
+            a.) the hotspot becomes a fork
+            b.) all child nodes become first worker if they are not marked as worker before
+            c.) if a child has dependence to more than one parent node, it will be marked as barrier
+        3.) if two barriers can run in parallel they are marked as barrierWorkers.
+            Two barriers can run in parallel if there is not a directed path from one to the other
+        """
+        for node in self.pet.graph.vertices():
+            #if (!node.childrenNodes.empty())
+            #   detectMWNode(node);
+            self.detect_task_parallelism(node)
+
+            if self.pet.graph.vp.mwType[node] == 'NONE':
+                self.pet.graph.vp.mwType[node] = 'ROOT'
+
+    def detect_task_parallelism(self, node: Vertex):
+        """The mainNode we want to compute the Task Parallelism Pattern for it
+        use Breadth First Search (BFS) to detect all barriers and workers.
+        1.) all child nodes become first worker if they are not marked as worker before
+        2.) if a child has dependence to more than one parent node, it will be marked as barrier
+        """
+# void PatternDetector::detectMWNode(Node &mainNode)
+# {
+#     // first insert all the direct children of mainnode in a queue to use it for the BFS
+#     queue<string> queueNode;
+#     for (const auto &node : mainNode.directSubNodes)
+#         queueNode.push(node);
+#
+#     while (!queueNode.empty())
+#     {
+#         // find the node and then go through all its child dependencies to mark them as WORKER or BARRIERS
+#         string nodeID = queueNode.front();
+#         queueNode.pop();
+#         Node &node = nodeMapComputed.find(nodeID)->second;// mainNode.mergedChildren.find(nodeID)->second;
+#         // a child node can be set to NONE or ROOT due a former detectMWNode call where it was the mainNode
+#         if (nodeMap.at(node.ID).mwType == NONE || nodeMap.at(node.ID).mwType == ROOT)
+#             nodeMap.at(node.ID).mwType = FORK;
+#
+#         // while using the node as the base child, we copy all the other children in a copy vector.
+#         // we do that because it could be possible that two children of the current node (two dependency) point to two different children of another child node which results that the childnode becomes BARRIER instead of WORKER
+#         // so we copy the whole other children in another vector and when one of the children of the current node does point to the other child node, we just adjust mwType and then we remove the node from the vector
+#         // Thus we prevent changing to BARRIER due of two dependencies pointing to two different children of the other node
+#
+#         // create the copy vector and delete the current node from it so that it only contains the other nodes
+#         vector<string> otherNodes = mainNode.directSubNodes;
+#         otherNodes.erase(std::remove(otherNodes.begin(), otherNodes.end(), nodeID), otherNodes.end());
+#
+#
+#         // All children are at least WORKER - if they have more then one parent then they are BARRIER
+#         for (auto dep : node.reverseRAWDepsOn)
+#         {
+#             // ignore dependencies pointing to the current node or one of its children
+#             if ((std::find(node.childrenNodes.begin(), node.childrenNodes.end(), dep.CUid) !=
+#                  node.childrenNodes.end()) || dep.CUid == node.ID)
+#                 continue;
+#
+#             // check if the dependency id is one of the subnodes of the directSubnodes
+#             for (const auto &otherNodesID : otherNodes)
+#             {
+#                 Node &otherNode = nodeMapComputed.find(otherNodesID)->second;
+#                 if (std::find(otherNode.childrenNodes.begin(), otherNode.childrenNodes.end(), dep.CUid) !=
+#                     otherNode.childrenNodes.end() || otherNode.ID == dep.CUid)
+#                 {
+#                     if (nodeMap.at(otherNode.ID).mwType == WORKER)
+#                         nodeMap.at(otherNode.ID).mwType = BARRIER;
+#                     else
+#                         nodeMap.at(otherNode.ID).mwType = WORKER;
+#                     // after setting just remove the childnode from the vector to prevent duplicate pointing to children of the child by the same other node
+#                     otherNodes.erase(std::remove(otherNodes.begin(), otherNodes.end(), otherNode.ID), otherNodes.end());
+#                     break;
+#                 }
+#             }
+#         }
+#     }
+#
+#     // check for Barrier Worker pairs
+#     // if two barriers don't have any dependency to each other then they create a barrierWorker pair
+#     // so check every barrier pair that they don't have a dependency to each other -> barrierWorker
+#     for (const auto &nodeID : mainNode.directSubNodes)
+#     {
+#         auto node = *nodeMapComputed.find(nodeID);
+#         //if (node.second.mwType == BARRIER) {
+#         if (nodeMap.at(node.first).mwType == BARRIER)
+#         {
+#             bool BW = true;
+#             for (const auto &nodeSecondID : mainNode.directSubNodes)
+#             {
+#                 if (nodeMap.at(node.first).mwType == BARRIER && nodeSecondID != node.first)
+#                 {
+#                     for (auto dep : node.second.RAWDepsOn)
+#                     {
+#                         if (dep.CUid == nodeSecondID)
+#                             goto NEXT_NODE;
+#                     }
+#                     for (auto dep : node.second.reverseRAWDepsOn)
+#                     {
+#                         if (dep.CUid == nodeSecondID)
+#                             goto NEXT_NODE;
+#                     }
+#                     // so these two nodes are BarrierWorker, because there is no dependency between them
+#                     vector<Node> pair = {node.second, nodeMapComputed.find(nodeSecondID)->second};
+#                     //mainNode.barrierWorkers.push_back(pair);
+#                     nodeMap.at(mainNode.ID).barrierWorkers.push_back(pair);
+#                     nodeMap.at(node.first).mwType = BARRIER_WORKER;
+#                     nodeMap.at(nodeSecondID).mwType = BARRIER_WORKER;
+#                 }
+#             }
+#             NEXT_NODE:;
+#         }
+#     }
+# }
+
     def detect_patterns(self):
         self.__detect_pipeline_loop()
         self.__detect_do_all_loop()
+
