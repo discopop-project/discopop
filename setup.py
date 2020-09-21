70f7f5a995581a9dfc197a52442f47ce3588eef6
Alexander Graf <17130992+aandergr@users.noreply.github.com>
2020-09-21 15:35:39 +0200

    setup.py: read install_requires from requirements.txt

--- setup.py
+++ setup.py
@@ -26,6 +26,11 @@ def get_version():
     raise SystemExit("Could not find version string.")
 
 
+def get_requirements():
+    with open(SRC / "requirements.txt") as f:
+        return [line.rstrip() for line in f]
+
+
 if sys.version_info < (3, 6):
     raise SystemExit("Discopop analyzer requires Python >= 3.6.")
 
@@ -41,16 +46,7 @@ setup(
                 "sequential program and makes recommendations on how to exploit it.",
     long_description=open(SRC / "README.md").read(),
     long_description_content_type="text/markdown",
-    install_requires=[
-        "matplotlib",
-        "networkx",
-        "pluginbase>=1.0.0",
-        "contextlib2>=0.5.5",
-        "docopt>=0.6.2",
-        "lxml>=4.3.3",
-        "numpy>=1.16.3",
-        "schema>=0.7.0",
-    ],
+    install_requires=get_requirements(),
     python_requires=">=3.6",
     entry_points={"console_scripts": ["graph_analyzer=graph_analyzer.__main__:main"]},
     zip_safe=True,
