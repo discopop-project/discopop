bcdde9f3c1ffc6428b3dc2891f38f33262b400e3
Alexander Graf <17130992+aandergr@users.noreply.github.com>
2020-10-14 12:54:20 +0200

    Include discopop_cpp in CI

--- .github/workflows/discopop_profiler/test.sh
+++ .github/workflows/discopop_profiler/test.sh
@@ -12,33 +12,34 @@ cd "$(dirname "$0")/../../.." || exit 1
 
 DISCOPOP_SRC=$(pwd)
 DISCOPOP_INSTALL="$(pwd)/build"
-CXX=clang++-8
+
+export PYTHONPATH=${DISCOPOP_SRC}
+export DISCOPOP_INSTALL
 
 tests="cu dependence reduction"
 
 function test_cu {
   # CU Generation
-  ${CXX} -g -O0 -fno-discard-value-names -Xclang -load -Xclang "${DISCOPOP_INSTALL}"/libi/LLVMCUGeneration.so -mllvm -fm-path -mllvm ./FileMapping.txt -c "$1" || return 1
+  python -m discopop_cpp -v --CUGeneration -c "$1" || return 1
 }
 
 function test_dependence {
   # Dependence Profiling
-  ${CXX} -g -O0 -fno-discard-value-names -Xclang -load -Xclang "${DISCOPOP_INSTALL}"/libi/LLVMDPInstrumentation.so -mllvm -fm-path -mllvm ./FileMapping.txt -c "$1" -o out.o || return 1
-  ${CXX} out.o -L"${DISCOPOP_INSTALL}"/rtlib -lDiscoPoP_RT -lpthread || return 1
+  python -m discopop_cpp -v --DPInstrumentation -c "$1" -o out.o || return 1
+  python -m discopop_cpp -v --DPInstrumentation out.o || return 1
   ./a.out || return 1
 }
 
 function test_reduction {
   # Identifying Reduction Operations
-  ${CXX} -g -O0 -fno-discard-value-names -Xclang -load -Xclang "${DISCOPOP_INSTALL}"/libi/LLVMDPReduction.so -mllvm -fm-path -mllvm ./FileMapping.txt -c "$1" -o out.o || return 1
-  ${CXX} out.o -L"${DISCOPOP_INSTALL}"/rtlib -lDiscoPoP_RT -lpthread || return 1
+  python -m discopop_cpp -v --DPReduction -c "$1" -o out.o || return 1
+  python -m discopop_cpp -v --DPReduction out.o || return 1
   ./a.out || return 1
 }
 
 exit_code=0
 for target in ./test/*/; do
   pushd $target
-  ${DISCOPOP_SRC}/scripts/dp-fmap
   for test in ${tests}; do
     echo "###"
     echo "### ${target} ${test}"
