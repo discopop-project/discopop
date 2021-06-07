43cd09e71abc2fa668fe64bb15f6719cc9dfc047
Lukas Rothenberger <lukas_rothenberger@hotmail.de>
2021-06-07 12:15:56 +0200

    minor

--- discopop_validation/executeDevTests.py
+++ discopop_validation/executeDevTests.py
@@ -7,7 +7,7 @@ if os.name != "posix":
 
 # execute unit tests and generate code coverage report
 print(colored("\n====== EXECUTE PYTEST ======", "blue"))
-os.system("coverage run --source=. -m unittest discover")
+os.system("coverage run --source=. -m unittest discover -s . -t ../..")
 print(colored("\n====== CODE COVERAGE REPORT ======", "blue"))
 os.system("coverage report -m --omit=executeDevTests.py")
 
