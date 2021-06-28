e2832d5e67808c14d9d1d44255cdde12a8e9e3b9
Raynard99 <raynard.widjaja@gmail.com>
2021-06-28 18:46:55 +0200

    run script change

--- run-poly.sh
+++ run-poly.sh
@@ -1,20 +1,20 @@
 /home/raynard/discopop/scripts/dp-fmap
 
 #CU Generation
-clang -g -O0 -fno-discard-value-names -Xclang -load -Xclang /home/raynard/discopop/build/libi/LLVMCUGeneration.so -mllvm -fm-path -mllvm ./FileMapping.txt -c 2mm.c
+clang -g -O0 -fno-discard-value-names -Xclang -load -Xclang /home/raynard/discopop/build/libi/LLVMCUGeneration.so -mllvm -fm-path -mllvm ./FileMapping.txt -c cholesky.c
 clang -g -O0 -fno-discard-value-names -Xclang -load -Xclang /home/raynard/discopop/build/libi/LLVMCUGeneration.so -mllvm -fm-path -mllvm ./FileMapping.txt -c polybench.c
 
 
 #Dependence Profiling
-clang -g -O0 -fno-discard-value-names -Xclang -load -Xclang /home/raynard/discopop/build/libi/LLVMDPInstrumentation.so -mllvm -fm-path -mllvm ./FileMapping.txt -c 2mm.c -o 2mm.o
+clang -g -O0 -fno-discard-value-names -Xclang -load -Xclang /home/raynard/discopop/build/libi/LLVMDPInstrumentation.so -mllvm -fm-path -mllvm ./FileMapping.txt -c cholesky.c -o cholesky.o
 clang -g -O0 -fno-discard-value-names -Xclang -load -Xclang /home/raynard/discopop/build/libi/LLVMDPInstrumentation.so -mllvm -fm-path -mllvm ./FileMapping.txt -c polybench.c -o polybench.o
-clang++ 2mm.o polybench.o -o 2mmpolybench -L /home/raynard/discopop/build/rtlib -lDiscoPoP_RT -lpthread
+clang++ cholesky.o polybench.o -o choleskypolybench -L /home/raynard/discopop/build/rtlib -lDiscoPoP_RT -lpthread
 
-./2mmpolybench
+./choleskypolybench
 
 #Reduction
-clang -g -O0 -fno-discard-value-names -Xclang -load -Xclang /home/raynard/discopop/build/libi/LLVMDPReduction.so -mllvm -fm-path -mllvm ./FileMapping.txt -c 2mm.c -o 2mm.o
+clang -g -O0 -fno-discard-value-names -Xclang -load -Xclang /home/raynard/discopop/build/libi/LLVMDPReduction.so -mllvm -fm-path -mllvm ./FileMapping.txt -c cholesky.c -o cholesky.o
 clang -g -O0 -fno-discard-value-names -Xclang -load -Xclang /home/raynard/discopop/build/libi/LLVMDPReduction.so -mllvm -fm-path -mllvm ./FileMapping.txt -c polybench.c -o polybench.o
-clang++ 2mm.o polybench.o -o 2mmpolybench_red -L /home/raynard/discopop/build/rtlib -lDiscoPoP_RT -lpthread
+clang++ cholesky.o polybench.o -o choleskypolybench_red -L /home/raynard/discopop/build/rtlib -lDiscoPoP_RT -lpthread
 
-./2mmpolybench_red
+./choleskypolybench_red
