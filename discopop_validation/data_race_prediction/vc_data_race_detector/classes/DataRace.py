5be8446432c000ef7d37b135a88b68a81e9a99de
Lukas Rothenberger <lukas_rothenberger@hotmail.de>
2021-12-12 23:24:40 +0100

    minor

--- discopop_validation/data_race_prediction/vc_data_race_detector/classes/DataRace.py
+++ discopop_validation/data_race_prediction/vc_data_race_detector/classes/DataRace.py
@@ -11,15 +11,13 @@ class DataRace(object):
     previous_writes: List[ScheduleElement]
 
     # represents a found data race for the output to the user
-    def __init__(self, section_id: int, schedule: Schedule, schedule_element: ScheduleElement, previous_writes: List[ScheduleElement]):
-        self.section_id: int = section_id
-        self.schedule = schedule
+    def __init__(self, schedule_element: ScheduleElement, previous_writes: List[ScheduleElement]):
         self.schedule_element: ScheduleElement = schedule_element
         self.previous_writes: List[ScheduleElement] = previous_writes
 
     def __str__(self):
         result_str = ""
-        result_str += colored("##### DATA RACE IN SECTION: " + str(self.section_id) + " #####\n", 'red', attrs=['bold'])
+        result_str += colored("##### DATA RACE: #####\n", 'red', attrs=['bold'])
         for write in self.previous_writes:
             result_str += "prev: " + str(write) + "\n"
         result_str += "===> " + str(self.schedule_element) + "\n"
@@ -52,7 +50,7 @@ class DataRace(object):
                 return operation.cu_id
         return "UNDEF"
 
-    def get_tuple(self) -> Tuple[int, ScheduleElement, List[ScheduleElement]]:
+    def get_tuples(self) -> Tuple[int, ScheduleElement, List[ScheduleElement]]:
         """returns the stored information as a tuple.
         Used for duplicate filtering."""
         return self.section_id, self.schedule_element, self.previous_writes
--- discopop_validation/data_race_prediction/vc_data_race_detector/core.py
+++ discopop_validation/data_race_prediction/vc_data_race_detector/core.py
@@ -1,7 +1,9 @@
+import copy
 from typing import List
 
 from discopop_validation.data_race_prediction.scheduler.classes.ScheduleElement import ScheduleElement
 from discopop_validation.data_race_prediction.scheduler.classes.SchedulingGraph import SchedulingGraph
+from discopop_validation.data_race_prediction.vc_data_race_detector.classes.DataRace import DataRace
 from discopop_validation.data_race_prediction.vc_data_race_detector.classes.State import State
 from discopop_validation.data_race_prediction.vc_data_race_detector.data_race_detector import goto_next_state
 
@@ -9,13 +11,11 @@ from discopop_validation.data_race_prediction.vc_data_race_detector.data_race_de
 def check_scheduling_graph(scheduling_graph: SchedulingGraph, dimensions: List[int]):
     # todo use dimensions to determine cutoff depth for task creation -> e.g. create tasks above 6 remaining levels
     initial_state = State(len(dimensions), scheduling_graph.lock_names, scheduling_graph.var_names)
-    __check_node(scheduling_graph, scheduling_graph.root_node_identifier, initial_state)
+    __check_node(scheduling_graph, scheduling_graph.root_node_identifier, initial_state, [])
 
 
-def __check_node(scheduling_graph: SchedulingGraph, node_identifier,state: State):
-
+def __check_node(scheduling_graph: SchedulingGraph, node_identifier, state: State, previous_writes: List[ScheduleElement]):
     node_schedule_element: ScheduleElement = scheduling_graph.graph.nodes[node_identifier]["data"]
-    #print("-->", node_schedule_element)
     if node_schedule_element is not None:
         # perform State update
         updated_state = goto_next_state(state, node_schedule_element)
@@ -23,8 +23,16 @@ def __check_node(scheduling_graph: SchedulingGraph, node_identifier,state: State
 
     for source, target in scheduling_graph.graph.out_edges(node_identifier):
         try:
-            __check_node(scheduling_graph, target, state)
+            state_copy = copy.copy(state)
+            previous_writes_copy = copy.copy(previous_writes)
+            if node_schedule_element is not None:
+                previous_writes_copy.append(node_schedule_element)
+            __check_node(scheduling_graph, target, state_copy, previous_writes_copy)
         except ValueError:
-            print("DATA RACE: TODO: create DR object", )
-
-    pass
+            if node_schedule_element is None:
+                # skip root element
+                continue
+            # todo DATA RACE: create DR object
+            data_race = DataRace(node_schedule_element, previous_writes)
+            # todo fix data race detection / data race reporting
+            print(data_race)
