0fc2a3de6ae1e22faddeffe5eaa494eabeb8054e
Lukas Rothenberger <lukas_rothenberger@hotmail.de>
2021-12-15 20:30:59 +0100

    added cutoff
    .

--- discopop_validation/data_race_prediction/vc_data_race_detector/core.py
+++ discopop_validation/data_race_prediction/vc_data_race_detector/core.py
@@ -8,14 +8,18 @@ from discopop_validation.data_race_prediction.vc_data_race_detector.classes.Stat
 from discopop_validation.data_race_prediction.vc_data_race_detector.data_race_detector import goto_next_state
 import concurrent.futures
 
-futures_cutoff_level = 6
+# todo arbitrary selected, might be tuned later on
+futures_cutoff_level = 14  # determines cutoff level (bottom-up)
 def check_scheduling_graph(scheduling_graph: SchedulingGraph, dimensions: List[int]) -> List[DataRace]:
     # todo use dimensions to determine cutoff depth for task creation -> e.g. create tasks above 6 remaining levels
     initial_state = State(len(dimensions), scheduling_graph.lock_names, scheduling_graph.var_names)
-    return __check_node(scheduling_graph, scheduling_graph.root_node_identifier, initial_state, [], 0)
+    graph_depth = 0
+    for entry in dimensions:
+        graph_depth += entry
+    return __check_node(scheduling_graph, scheduling_graph.root_node_identifier, initial_state, [], 0, graph_depth)
 
 
-def __check_node(scheduling_graph: SchedulingGraph, node_identifier, state: State, previous_writes: List[ScheduleElement], level) -> List[DataRace]:
+def __check_node(scheduling_graph: SchedulingGraph, node_identifier, state: State, previous_writes: List[ScheduleElement], level, graph_depth) -> List[DataRace]:
     global futures_cutoff_level
     node_schedule_element: ScheduleElement = scheduling_graph.graph.nodes[node_identifier]["data"]
     if node_schedule_element is not None:
@@ -30,7 +34,7 @@ def __check_node(scheduling_graph: SchedulingGraph, node_identifier, state: Stat
     data_races = []
 
     # check if multithreading should be used
-    if level <= futures_cutoff_level:
+    if level <= graph_depth - futures_cutoff_level:
         futures = []
         with concurrent.futures.ThreadPoolExecutor() as executor:
             for source, target in scheduling_graph.graph.out_edges(node_identifier):
@@ -39,7 +43,7 @@ def __check_node(scheduling_graph: SchedulingGraph, node_identifier, state: Stat
                 if node_schedule_element is not None:
                     if node_schedule_element.contains_write():
                         previous_writes_copy.append(node_schedule_element)
-                futures.append(executor.submit(__check_node, scheduling_graph, target, state_copy, previous_writes_copy, level+1))
+                futures.append(executor.submit(__check_node, scheduling_graph, target, state_copy, previous_writes_copy, level+1, graph_depth))
                 #data_races += __check_node(scheduling_graph, target, state_copy, previous_writes_copy)
         for future in futures:
             if future.result() is not None:
@@ -52,5 +56,5 @@ def __check_node(scheduling_graph: SchedulingGraph, node_identifier, state: Stat
             if node_schedule_element is not None:
                 if node_schedule_element.contains_write():
                     previous_writes_copy.append(node_schedule_element)
-            data_races += __check_node(scheduling_graph, target, state_copy, previous_writes_copy, level+1)
+            data_races += __check_node(scheduling_graph, target, state_copy, previous_writes_copy, level+1, graph_depth)
     return data_races
