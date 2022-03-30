629f4983870bb16d711c150450a3cc46ff16e06d
Lukas Rothenberger <lukas_rothenberger@hotmail.de>
2022-03-30 13:55:11 +0200

    added drb079

--- discopop_validation/run_validation.sh
+++ discopop_validation/run_validation.sh
@@ -68,4 +68,7 @@ echo ""
 #python -m discopop_validation --path=${PWD}/discopop_validation/test/code_samples/simple_tasking/drb072 --dep-file=out_dep.txt --ll-file=out_dp_inst.ll --verbose=true --dp-build-path=/home/lukas/git/discopop/build/ --thread-count=7  --omp-pragmas-file=pragmas.omp #--json=original_suggestions.json --validation-time-limit=3
 
 # DRB078
-python -m discopop_validation --path=${PWD}/discopop_validation/test/code_samples/simple_tasking/drb078 --dep-file=out_dep.txt --ll-file=out_dp_inst.ll --verbose=true --dp-build-path=/home/lukas/git/discopop/build/ --thread-count=7  --omp-pragmas-file=pragmas.omp #--json=original_suggestions.json --validation-time-limit=3
+#python -m discopop_validation --path=${PWD}/discopop_validation/test/code_samples/simple_tasking/drb078 --dep-file=out_dep.txt --ll-file=out_dp_inst.ll --verbose=true --dp-build-path=/home/lukas/git/discopop/build/ --thread-count=7  --omp-pragmas-file=pragmas.omp #--json=original_suggestions.json --validation-time-limit=3
+
+# DRB079
+python -m discopop_validation --path=${PWD}/discopop_validation/test/code_samples/simple_tasking/drb079 --dep-file=out_dep.txt --ll-file=out_dp_inst.ll --verbose=true --dp-build-path=/home/lukas/git/discopop/build/ --thread-count=7  --omp-pragmas-file=pragmas.omp #--json=original_suggestions.json --validation-time-limit=3
