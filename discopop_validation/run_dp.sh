2489e65581efcae4ba6ef9c13dbbfc89fd79a3fb
Lukas Rothenberger <lukas_rothenberger@hotmail.de>
2022-05-10 17:36:23 +0200

    .

--- discopop_validation/run_dp.sh
+++ discopop_validation/run_dp.sh
@@ -3,4 +3,4 @@ cd ..
 TARGET=$1
 echo "TARGET: $TARGET"
 
-timeout 80s python -m discopop_validation --path=${PWD}/discopop_validation/test/code_samples/discopop/$TARGET --dep-file=out_dep.txt --ll-file=out_dp_inst.ll --verbose=true --dp-build-path=/home/lukas/git/discopop/build/ --thread-count=7  --json=original_suggestions.json  --data-race-output=${PWD}/discopop_validation/test/code_samples/discopop/$TARGET/data_races.txt
+timeout 80s python -m discopop_validation --path=${PWD}/discopop_validation/test/code_samples/discopop/$TARGET --dep-file=out_dep.txt --ll-file=out_dp_inst.ll --verbose=true --dp-build-path=/home/lukas/git/discopop/build/ --thread-count=1  --json=original_suggestions.json  --data-race-output=${PWD}/discopop_validation/test/code_samples/discopop/$TARGET/data_races.txt
