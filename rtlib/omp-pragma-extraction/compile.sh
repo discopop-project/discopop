cfee94e8f287a4d8567a318f422e34d7e34b07d7
Lukas Rothenberger <lukas.rothenberger@tu-darmstadt.de>
2022-08-15 17:24:15 +0200

    compile simple-alias-detection

--- rtlib/omp-pragma-extraction/compile.sh
+++ rtlib/omp-pragma-extraction/compile.sh
@@ -1,14 +1,7 @@
-#CURRENT_DIR = "$1"
-#BUILD_DIR = "$2"
+#CMAKE_CURRENT_SOURCE_DIR = "$1"
+#CMAKE_CURRENT_BINARY_DIR = "$2"
 
-echo "CURRENT: $1"
-echo "BUILD: $2"
-echo "CMAKE_CURRENT_BINARY_DIR: $3"
-
-echo "Compiling omp-pragma-extraction."
-
-mkdir $3/omp-pragma-extraction
-cd $3/omp-pragma-extraction
+mkdir $2/omp-pragma-extraction
+cd $2/omp-pragma-extraction
 cmake $1/omp-pragma-extraction
 make
-# touch $BUILD_DIR/ALIVE.txt
