3b1e3754f0f63cb7f8ab94dd8ae3f1a5e2140179
Lukas Rothenberger <lukas.rothenberger@tu-darmstadt.de>
2022-08-16 15:14:23 +0200

    removed dead code

--- discopop_validation/source_code_modifications/CodeDifferences.py
+++ discopop_validation/source_code_modifications/CodeDifferences.py
@@ -59,9 +59,8 @@ def main():
 
 
 def file_difference_checker(original_file: str, modified_file: str) -> Tuple[Dict[int, int], bool]:
-    #line_mapping = get_line_mapping(original_file, modified_file)
     # new line mapping
-    line_mapping = get_new_line_mapping(original_file, modified_file)
+    line_mapping = get_line_mapping(original_file, modified_file)
 
     profiling_required = get_profiling_necessity(original_file, modified_file)
 
@@ -160,130 +159,6 @@ def get_profiling_necessity(original_file: str, modified_file: str) -> bool:
 
 
 def get_line_mapping(original_file: str, modified_file: str) -> Dict[int, int]:
-    original = open(original_file, "r")
-    original_file_line_numbers = range(1, len(original.readlines()) + 1)
-    original.close()
-    original = open(original_file, "r")
-    original_path = os.path.realpath(original.name)
-    modified = open(modified_file, "r")
-    modified_path = os.path.realpath(modified.name)
-    diff_lines = unified_diff(original.readlines(), modified.readlines(), fromfile=original_path, tofile=modified_path,
-                              n=0)
-    original.close()
-    modified.close()
-
-    modification_scopes: List[Tuple[str, str]] = []
-    added_lines_dict: Dict[Tuple[str, str], List[Tuple[int, str]]] = dict()
-    removed_lines_dict: Dict[Tuple[str, str], List[Tuple[int, str]]] = dict()
-    removed_line_distance = -1
-    added_line_distance = -1
-
-    # read raw modifications from diff
-
-    for line in diff_lines:
-        line = line.replace("\n", "")
-        if line.startswith("@@"):
-            # get line numbers
-            split_line = line.split(" ")
-            start_line_original = split_line[1].replace("-", "")
-            start_line_modified = split_line[2].replace("+", "")
-            # ignore column numbers
-            if "," in start_line_original:
-                start_line_original = start_line_original[:start_line_original.index(",")]
-            if "," in start_line_modified:
-                start_line_modified = start_line_modified[:start_line_modified.index(",")]
-            start_line_original = int(start_line_original)
-            start_line_modified = int(start_line_modified)
-            modification_scopes.append((start_line_original, start_line_modified))
-            removed_lines_dict[(start_line_original, start_line_modified)] = []
-            added_lines_dict[(start_line_original, start_line_modified)] = []
-            removed_line_distance = -1
-            added_line_distance = -1
-
-        if line.startswith("-") and not line.startswith("---"):
-            # line removed from original file
-            removed_line_distance += 1
-            removed_line_number = int(modification_scopes[-1][0]) + removed_line_distance
-            removed_lines_dict[modification_scopes[-1]].append((removed_line_number, line[1:]))
-        if line.startswith("+") and not line.startswith("+++"):
-            # line added to original file
-            added_line_distance += 1
-            added_line_number = int(modification_scopes[-1][1]) + added_line_distance
-            added_lines_dict[modification_scopes[-1]].append((added_line_number, line[1:]))
-
-    # create line mapping and extract line mapping rules
-    line_mapping: Dict[int, int] = dict()
-    line_mapping_rules: List[Tuple[int, int]] = []  # (boundary line, difference )
-
-    # gather add and removed lines information
-    added_lines_after_dict = dict()
-    for key in added_lines_dict:
-        for entry in added_lines_dict[key]:
-            if key[0] not in added_lines_after_dict:
-                added_lines_after_dict[key[0]] = 0
-            added_lines_after_dict[key[0]] += 1
-    removed_lines_after_dict = dict()
-    for key in removed_lines_dict:
-        for entry in removed_lines_dict[key]:
-            if key[0] - 1 not in removed_lines_after_dict:
-                removed_lines_after_dict[key[0] - 1] = 0
-            removed_lines_after_dict[key[0] - 1] -= 1
-
-    # create line mapping rules based on added_lines_after_dict
-    for line_num in sorted(added_lines_after_dict, reverse=True):
-        line_mapping_rules.append((line_num, added_lines_after_dict[line_num]))
-    # create line mapping rules based on removed_lines_after_dict
-    for line_num in sorted(removed_lines_after_dict, reverse=True):
-        line_mapping_rules.append((line_num, removed_lines_after_dict[line_num]))
-
-    print("Line Mapping Rules")
-    print(line_mapping_rules)
-
-    # create line mapping rules based on cleaned information
-    #    for key in added_lines_dict:
-    #        for entry in added_lines_dict[key]:
-    #            line_mapping_rules.append((entry[0], 1))  # add one line
-    #    for key in removed_lines_dict:
-    #        for entry in removed_lines_dict[key]:
-    #            line_mapping_rules.append((entry[0], -1))  # remove one line
-
-    # add lines of original file to line mapping
-    for line_num in original_file_line_numbers:
-        line_mapping[line_num] = line_num
-
-    #    # add modification scopes to line mapping
-    #    for mod_scope in modification_scopes:
-    #        # line_mapping[mod_scope[0]] = mod_scope[1]
-    #        line_mapping_rules.append((mod_scope[0]+1, mod_scope[1] - mod_scope[0]))
-
-    # apply line mapping rules:
-    for rule_boundary_line, rule_line_difference in line_mapping_rules:
-        for line_num in line_mapping:
-            # apply rule
-            if rule_line_difference > 0:
-                if line_num > rule_boundary_line:
-                    line_mapping[line_num] = line_mapping[line_num] + rule_line_difference
-            else:
-                if line_num > rule_boundary_line:
-                    line_mapping[line_num] = line_mapping[line_num] + rule_line_difference
-
-
-# old implementation
-#    for rule_boundary_line, rule_line_difference in line_mapping_rules:
-#        for line_num in line_mapping:
-#            # apply rule
-#            if rule_line_difference > 0:
-#                if line_mapping[line_num] > rule_boundary_line:
-#                    line_mapping[line_num] = line_mapping[line_num] + rule_line_difference
-#            else:
-#                if line_mapping[line_num] > rule_boundary_line:
-#                    line_mapping[line_num] = line_mapping[line_num] + rule_line_difference
-
-# add removed and added lines to line mapping
-    return line_mapping
-
-
-def get_new_line_mapping(original_file: str, modified_file: str) -> Dict[int, int]:
     import os
     stream = os.popen("diff --old-line-format='-$$$ %L\n' --new-line-format='+$$$ %L\n' --unchanged-line-format='|$$$ %L\n' " + original_file + " " + modified_file)
     # $$$ used to identify the amount of operations per line
@@ -297,9 +172,7 @@ def get_new_line_mapping(original_file: str, modified_file: str) -> Dict[int, in
         line = line.replace("\n", "")
         if len(line) == 0:  # empty lines required to split old and new line in case of modifications
             continue
-        print(original_line_num, "\t", current_offset, "\t", line)
         # always write to line_mapping prior to modifying original_line_num
-
 
         # if line has been added, original_line_num is not increased as it was not present in the original code
         if line.startswith("|"):
--- discopop_validation/source_code_modifications/test_original.c
+++ discopop_validation/source_code_modifications/test_original.c
@@ -1,2 +1,3 @@
 int x = 42;
-int y = 12;
+int y = 3;
+int z = 142;
