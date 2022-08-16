fd8e4a1879ed33af6110a0dbc5960217ec6e0c45
Lukas Rothenberger <lukas.rothenberger@tu-darmstadt.de>
2022-08-16 13:04:44 +0200

    modified test cases

---
+++ discopop_validation/source_code_modifications/test/source_code_modifications/add/two_lines/modified.c
@@ -0,0 +1,4 @@
+int x = 42;
+int a = 2;
+int b = 3;
+int z = 142;
--- discopop_validation/source_code_modifications/test/source_code_modifications/tests.py
+++ discopop_validation/source_code_modifications/test/source_code_modifications/tests.py
@@ -5,9 +5,9 @@ from discopop_validation.source_code_modifications.CodeDifferences import file_d
 
 
 class TestSourceCodeModifications(unittest.TestCase):
-    def test_add_line_prior(self):
+    def test_add_line_before(self):
         path = os.path.dirname(os.path.abspath(__file__))
-        dir_name = "add/line_prior"
+        dir_name = "add/line_before"
         original_file = path + "/" + dir_name + "/original.c"
         modified_file = path + "/" + dir_name + "/modified.c"
         line_mapping, profiling_required = file_difference_checker(original_file, modified_file)
@@ -38,6 +38,90 @@ class TestSourceCodeModifications(unittest.TestCase):
         modified_file = path + "/" + dir_name + "/modified.c"
         line_mapping, profiling_required = file_difference_checker(original_file, modified_file)
         expected_line_mapping = {
+        }
+        self.assertEqual(line_mapping, expected_line_mapping)
+        self.assertTrue(profiling_required)
+
+
+    def test_add_two_lines(self):
+        path = os.path.dirname(os.path.abspath(__file__))
+        dir_name = "add/two_lines"
+        original_file = path + "/" + dir_name + "/original.c"
+        modified_file = path + "/" + dir_name + "/modified.c"
+        line_mapping, profiling_required = file_difference_checker(original_file, modified_file)
+        expected_line_mapping = {
+            2: 4
+        }
+        self.assertEqual(line_mapping, expected_line_mapping)
+        self.assertTrue(profiling_required)
+
+
+    def test_add_two_lines_distributed(self):
+        path = os.path.dirname(os.path.abspath(__file__))
+        dir_name = "add/two_lines_distributed"
+        original_file = path + "/" + dir_name + "/original.c"
+        modified_file = path + "/" + dir_name + "/modified.c"
+        line_mapping, profiling_required = file_difference_checker(original_file, modified_file)
+        expected_line_mapping = {
+            2: 3,
+            3: 5
+        }
+        self.assertEqual(line_mapping, expected_line_mapping)
+        self.assertTrue(profiling_required)
+
+
+    def test_remove_line(self):
+        path = os.path.dirname(os.path.abspath(__file__))
+        dir_name = "remove/line"
+        original_file = path + "/" + dir_name + "/original.c"
+        modified_file = path + "/" + dir_name + "/modified.c"
+        line_mapping, profiling_required = file_difference_checker(original_file, modified_file)
+        expected_line_mapping = {
+            2: -1,
+            3: 2
+        }
+        self.assertEqual(line_mapping, expected_line_mapping)
+        self.assertTrue(profiling_required)
+
+    def test_remove_last_line(self):
+        path = os.path.dirname(os.path.abspath(__file__))
+        dir_name = "remove/last_line"
+        original_file = path + "/" + dir_name + "/original.c"
+        modified_file = path + "/" + dir_name + "/modified.c"
+        line_mapping, profiling_required = file_difference_checker(original_file, modified_file)
+        expected_line_mapping = {
+            2: -1
+        }
+        self.assertEqual(line_mapping, expected_line_mapping)
+        self.assertTrue(profiling_required)
+
+
+    def test_remove_two_lines(self):
+        path = os.path.dirname(os.path.abspath(__file__))
+        dir_name = "remove/two_lines"
+        original_file = path + "/" + dir_name + "/original.c"
+        modified_file = path + "/" + dir_name + "/modified.c"
+        line_mapping, profiling_required = file_difference_checker(original_file, modified_file)
+        expected_line_mapping = {
+            2: -1,
+            3: -1,
+            4: 2
+        }
+        self.assertEqual(line_mapping, expected_line_mapping)
+        self.assertTrue(profiling_required)
+
+
+    def test_remove_two_lines_distributed(self):
+        path = os.path.dirname(os.path.abspath(__file__))
+        dir_name = "remove/two_lines_distributed"
+        original_file = path + "/" + dir_name + "/original.c"
+        modified_file = path + "/" + dir_name + "/modified.c"
+        line_mapping, profiling_required = file_difference_checker(original_file, modified_file)
+        expected_line_mapping = {
+            2: -1,
+            3: 2,
+            4: -1,
+            5: 3
         }
         self.assertEqual(line_mapping, expected_line_mapping)
         self.assertTrue(profiling_required)
