ddf9d1f8673f0d493e605de8388724cb1a79cb95
Lukas Rothenberger <lukas.rothenberger@tu-darmstadt.de>
2022-08-22 13:43:22 +0200

    updated scripts

--- discopop_validation/test/code_samples/drb/006/run_dp_maker.sh
+++ discopop_validation/test/code_samples/drb/006/run_dp_maker.sh
@@ -18,4 +18,6 @@ cd $DP_MAKER_DIR
 python -m Makefile_Analyzer --target-project=$TARGET_PROJECT_FOLDER --target-makefile=$TARGET_FOLDER/Makefile --dp-path=/home/lukas/git/discopop/ --dp-bui=/home/lukas/git/discopop/build/ --exec-mode=dp_red
 cd $TARGET_FOLDER && make -j3 -f tmp_makefile.mk && ./out
 
+./create_suggestions.sh
+
 cd $HOME_DIR
