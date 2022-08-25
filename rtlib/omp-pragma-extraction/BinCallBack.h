7638335a89bed50fe3dc5df93f02b5d7f57ba0e5
Lukas Rothenberger <lukas.rothenberger@tu-darmstadt.de>
2022-08-25 14:22:23 +0200

    recursive extraction of pragma scopes from AST

--- rtlib/omp-pragma-extraction/BinCallBack.h
+++ rtlib/omp-pragma-extraction/BinCallBack.h
@@ -11,6 +11,31 @@ using namespace clang;
 
 class BinCallBack: public MatchFinder::MatchCallback {
 public:
+
+    SourceLocation getScopeEndLineFromChildren(const Stmt* root, SourceLocation lastLoc, SourceManager& SM){
+        // check if root updates lastLoc
+        BeforeThanCompare<SourceLocation> isBefore(SM);
+        if(isBefore(lastLoc, root->getSourceRange().getEnd())){
+            // scope of root extends last known scope
+            lastLoc = root->getSourceRange().getEnd();
+        }
+        // search in captured statement
+        if(isa<CapturedStmt>(root)){
+            const Stmt* captd = cast<CapturedStmt>(root)->getCapturedStmt();
+            // enter recursion for captd
+            lastLoc = getScopeEndLineFromChildren(captd, lastLoc, SM);
+        }
+        // search in children
+        for(auto child : root->children()){
+            if(! child){
+                continue;
+            }
+            // enter recursion
+            lastLoc = getScopeEndLineFromChildren(child, lastLoc, SM);
+        }
+        return lastLoc;
+    }
+
     void run(const MatchFinder::MatchResult &r) override {
         const clang::Stmt* ompDirective = r.Nodes.getNodeAs<Stmt>("ompPragma");
         ASTContext* ctx = r.Context;
@@ -21,9 +46,13 @@ public:
         bool found_captured_stmt = false;
         for(auto child : ompDirective->children()){
             if(isa<CapturedStmt>(child)){
+                // get begin location and write to file
                 child->getSourceRange().getBegin().print(llvm::outs(), ctx->getSourceManager());
                 llvm::outs() << ";";
-                child->getSourceRange().getEnd().print(llvm::outs(), ctx->getSourceManager());
+                // get end location by recursively checking children and write to file
+                SourceLocation endLoc;
+                endLoc = getScopeEndLineFromChildren(child, child->getSourceRange().getBegin(), ctx->getSourceManager());
+                endLoc.print(llvm::outs(), ctx->getSourceManager());
                 llvm::outs() << "\n";
                 found_captured_stmt = true;
                 break;
