66b8191a2f2a56ed9652ef808d6b37045d614106
Lukas Rothenberger <lukas.rothenberger@tu-darmstadt.de>
2022-11-15 11:50:11 +0100

    Update discopop_wizard/screens/main.py

--- discopop_wizard/screens/main.py
+++ discopop_wizard/screens/main.py
@@ -28,7 +28,6 @@ def push_main_screen(manager: ptg.WindowManager, config_dir: str, wizard):
         .set_title("[210 bold]DiscoPoP Configuration Wizard")
     )
     body.overflow = ptg.Overflow.SCROLL
-    #manager.add(body, assign="body_left")
 
     buttons = (ptg.Window(
         ["Add Configuration", lambda *_: push_add_configuration_screen(manager, config_dir, wizard)],
