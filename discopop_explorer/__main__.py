4c62a7018e7eaca2ca4e57fb304ed6cce6af136f
Lukas Rothenberger <lukas.rothenberger@tu-darmstadt.de>
2022-12-16 09:51:20 +0100

    updated python dependencies

--- discopop_explorer/__main__.py
+++ discopop_explorer/__main__.py
@@ -40,7 +40,7 @@ import cProfile
 import io
 import json
 import os
-import pstats  # type:ignore
+import pstats2  # type:ignore
 import sys
 import time
 
@@ -192,7 +192,7 @@ def main():
         if os.path.exists("profiling_stats.txt"):
             os.remove("profiling_stats.txt")
         with open("profiling_stats.txt", "w+") as f:
-            stats = pstats.Stats(profile, stream=f).sort_stats(pstats.SortKey.TIME).reverse_order()
+            stats = pstats2.Stats(profile, stream=f).sort_stats(pstats2.SortKey.TIME).reverse_order()
             stats.print_stats()
 
     print("Time taken for pattern detection: {0}".format(end - start))
