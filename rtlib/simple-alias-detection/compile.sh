e45f2bac25c5277f63995a09c4710b925a405611
Lukas Rothenberger <lukas.rothenberger@tu-darmstadt.de>
2022-12-21 15:06:46 +0100

    cleanup

--- rtlib/simple-alias-detection/compile.sh
+++ rtlib/simple-alias-detection/compile.sh
@@ -1,3 +1,12 @@
+# This file is part of the DiscoPoP software (http://www.discopop.tu-darmstadt.de)
+#
+# Copyright (c) 2020, Technische Universitaet Darmstadt, Germany
+#
+# This software may be modified and distributed under the terms of
+# the 3-Clause BSD License.  See the LICENSE file in the package base
+# directory for details.
+
+
 #CMAKE_CURRENT_SOURCE_DIR = "$1"
 #CMAKE_CURRENT_BINARY_DIR = "$2"
 
