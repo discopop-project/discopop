e4f497b9733dece157f9f876adf8bb3fb54801da
Lukas Rothenberger <lukas.rothenberger@tu-darmstadt.de>
2023-02-03 10:58:38 +0100

    basic code generator

---
+++ discopop_library/CodeGenerator/classes/Line.py
@@ -0,0 +1,24 @@
+# This file is part of the DiscoPoP software (http://www.discopop.tu-darmstadt.de)
+#
+# Copyright (c) 2020, Technische Universitaet Darmstadt, Germany
+#
+# This software may be modified and distributed under the terms of
+# the 3-Clause BSD License.  See the LICENSE file in the package base
+# directory for details.
+
+from typing import Optional, List
+
+
+class Line(object):
+    line_num: Optional[int]
+    content: str
+    owns_region: Optional[int]
+    belongs_to_regions: List[int]
+    belongs_to_original_line: int
+
+    def __init__(self, parent_line_num: int, line_num=None, content=""):
+        self.line_num = line_num
+        self.content = content
+        self.owns_region = None
+        self.belongs_to_regions = []
+        self.belongs_to_original_line = parent_line_num
