058c8b3681c25c766f2c5fb846ff56e302e0507c
Lukas Rothenberger <lukas.rothenberger@tu-darmstadt.de>
2023-02-10 16:24:33 +0100

    added license tags

--- discopop_explorer/pattern_detectors/combined_gpu_patterns/classes/Dependency.py
+++ discopop_explorer/pattern_detectors/combined_gpu_patterns/classes/Dependency.py
@@ -1,3 +1,11 @@
+# This file is part of the DiscoPoP software (http://www.discopop.tu-darmstadt.de)
+#
+# Copyright (c) 2020, Technische Universitaet Darmstadt, Germany
+#
+# This software may be modified and distributed under the terms of
+# the 3-Clause BSD License.  See the LICENSE file in the package base
+# directory for details.
+
 from typing import Set
 
 from discopop_explorer.pattern_detectors.combined_gpu_patterns.classes.Aliases import (
--- discopop_explorer/pattern_detectors/combined_gpu_patterns/prepare_metadata.py
+++ discopop_explorer/pattern_detectors/combined_gpu_patterns/prepare_metadata.py
@@ -1,3 +1,11 @@
+# This file is part of the DiscoPoP software (http://www.discopop.tu-darmstadt.de)
+#
+# Copyright (c) 2020, Technische Universitaet Darmstadt, Germany
+#
+# This software may be modified and distributed under the terms of
+# the 3-Clause BSD License.  See the LICENSE file in the package base
+# directory for details.
+
 import sys
 from typing import Set, List, Tuple
 
