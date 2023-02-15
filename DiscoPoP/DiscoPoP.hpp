e4b8223c757fffdfb08335ea5d75e3ce6aecdc35
Lukas Rothenberger <lukas.rothenberger@tu-darmstadt.de>
2023-02-15 14:06:34 +0100

    minor fix: detection of reduction operations (decrementing)

--- DiscoPoP/DiscoPoP.hpp
+++ DiscoPoP/DiscoPoP.hpp
@@ -405,7 +405,7 @@ namespace {
         }
         unsigned dp_reduction_get_file_id(llvm::Function *func);
         bool dp_reduction_init_util(std::string fmap_path);
-        char dp_reduction_get_char_for_opcode(unsigned opcode);
+        char dp_reduction_get_char_for_opcode(llvm::Instruction *instr);
         bool dp_reduction_is_operand(llvm::Instruction *instr, llvm::Value *operand);
         int dp_reduction_get_op_order(char c);
         Type *dp_reduction_pointsToStruct(PointerType *PTy);
