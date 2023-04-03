845c723bb56348d049725ad7b68ce89193cd8e56
Lukas Rothenberger <lukas.rothenberger@tu-darmstadt.de>
2023-04-03 11:46:00 +0200

    remove dependencies, if they are overwritten by a more specific .._II_x

--- discopop_explorer/PETGraphX.py
+++ discopop_explorer/PETGraphX.py
@@ -623,6 +623,7 @@ class PETGraphX(object):
                         continue
                     if sink_cu_id and source_cu_id:
                         g.add_edge(sink_cu_id, source_cu_id, data=parse_dependency(dep))
+
         return cls(g, reduction_vars, pos)
 
     def calculateFunctionMetadata(self) -> None:
@@ -643,6 +644,34 @@ class PETGraphX(object):
 
             func_node.calculate_reachability_pairs(self)
         print("Metadata calculation done.")
+
+        # cleanup dependencies (remove dependencies, if it is overwritten by a more specific Intra-iteration dependency
+        print("Cleaning dependencies...")
+        to_be_removed = []
+        for cu_node in self.all_nodes(CUNode):
+            out_deps = self.out_edges(cu_node.id, EdgeType.DATA)
+            for dep_1 in out_deps:
+                for dep_2 in out_deps:
+                    if dep_1 == dep_2:
+                        continue
+                    if dep_1[2].dtype == dep_2[2].dtype and dep_1[2].etype == dep_2[2].etype and dep_1[2].memory_region == dep_2[2].memory_region and dep_1[2].sink_line == dep_2[2].sink_line and \
+                        dep_1[2].source_line == dep_2[2].source_line and dep_1[2].var_name == dep_2[2].var_name:
+                        if not dep_1[2].intra_iteration and dep_2[2].intra_iteration:
+                            # dep_2 is a more specific duplicate of dep_1
+                            # remove dep_1
+                            to_be_removed.append(dep_1)
+
+        to_be_removed_with_keys = []
+        for dep in to_be_removed:
+            graph_edges = self.g.out_edges(dep[0], keys=True, data="data")
+
+            for s, t, key, data in graph_edges:
+                if dep[0] == s and dep[1] == t and dep[2] == data:
+                    to_be_removed_with_keys.append((s, t, key))
+        for edge in set(to_be_removed_with_keys):
+            self.g.remove_edge(edge[0], edge[1], edge[2])
+        print("Cleaning dependencies done.")
+
 
     def show(self):
         """Plots the graph
