a9aa1dab944fd2560903d78436de245a5e193533
Lukas Rothenberger <lukas.rothenberger@tu-darmstadt.de>
2023-04-23 17:18:37 +0200

    basic creation of models

--- discopop_library/OptimizationGraph/CostModels/utilities.py
+++ discopop_library/OptimizationGraph/CostModels/utilities.py
@@ -1,3 +1,4 @@
+import copy
 from typing import List, Dict, cast, Optional
 
 import networkx as nx  # type: ignore
@@ -15,11 +16,11 @@ def get_performance_models_for_functions(graph: nx.DiGraph) -> Dict[FunctionRoot
     for node_id in graph.nodes:
         node_data = graph.nodes[node_id]["data"]
         if isinstance(node_data, FunctionRoot):
-            performance_models[node_data] = get_node_performance_model(graph, node_id)
+            performance_models[node_data] = get_node_performance_models(graph, node_id)
     return performance_models
 
 
-def get_node_performance_model(graph: nx.DiGraph, node_id: int) -> List[CostModel]:
+def get_node_performance_models(graph: nx.DiGraph, node_id: int) -> List[CostModel]:
     """Returns the performance models for the given node"""
     result_list: List[CostModel] = []
     successors = get_successors(graph, node_id)
@@ -27,34 +28,67 @@ def get_node_performance_model(graph: nx.DiGraph, node_id: int) -> List[CostMode
     node_data = cast(GenericNode, data_at(graph, node_id))
 
     # consider performance models of children
-    model_plus_children = node_data.get_cost_model().plus_combine(get_performance_model_for_children(graph, node_id))
+    children_models = get_performance_models_for_children(graph, node_id)
 
-    # construct the performance model
+    if len(children_models) == 0:
+        children_models = [node_data.get_cost_model()]
+    else:
+        tmp_node_cost_model = node_data.get_cost_model()
+        for idx, child_model in enumerate(children_models):
+            children_models[idx] = child_model.plus_combine(tmp_node_cost_model)
+
+    # construct the performance models
     if successor_count >= 1:
-        for successor in successors:
-            combined_model = model_plus_children
-            # add transfer costs
-            transfer_costs_model = get_edge_data(graph, node_id, successor).get_cost_model()
-            combined_model = combined_model.plus_combine(transfer_costs_model)
-            # append the model of the successor
-            for model in get_node_performance_model(graph, successor):
-                result_list.append(combined_model.plus_combine(model))
+        for children_model in children_models:
+            for successor in successors:
+                combined_model = children_model
+                # add transfer costs
+                transfer_costs_model = get_edge_data(graph, node_id, successor).get_cost_model()
+                combined_model = combined_model.plus_combine(transfer_costs_model)
+                # append the model of the successor
+                for model in get_node_performance_models(graph, successor):
+                    result_list.append(combined_model.plus_combine(model))
         return result_list
 
     # successor count == 0 or successor count > 1
-    return [model_plus_children]
+    return children_models
 
 
-def get_performance_model_for_children(graph: nx.DiGraph, node_id: int) -> Optional[CostModel]:
+def get_performance_models_for_children(graph: nx.DiGraph, node_id: int) -> List[CostModel]:
     """Construct a performance model for the children of the given node, or return None if no children exist"""
     # todo: consider children
+    child_models: List[CostModel] = []
+
+    # create all combinations for models from children
+    first_iteration = True
+    for child_id in get_children(graph, node_id):
+        if first_iteration:
+            first_iteration = False
+            for model in get_node_performance_models(graph, child_id):
+                # initialize list of child models
+                child_models.append(model)
+        else:
+            # create "product set" of child models
+            product_set = []
+            for model in get_node_performance_models(graph, child_id):
+                temp_models = [cm.plus_combine(model) for cm in child_models]
+                product_set += temp_models
+            child_models = product_set
+    pass
+    return child_models
+
+
+
+
+
+
     children_models: List[CostModel] = []
     for child_id in get_children(graph, node_id):
-        for model in get_node_performance_model(graph, child_id):
+        for model in get_node_performance_models(graph, child_id):
             children_models.append(model)
     # construct model from individual performance models of the children
     if len(children_models) == 0:
-        return None
+        return []
     else:
         model = children_models[0]
         for child_model in children_models[1:]:
--- discopop_library/OptimizationGraph/OptimizationGraph.py
+++ discopop_library/OptimizationGraph/OptimizationGraph.py
@@ -1,7 +1,7 @@
 from typing import Dict
 
 import networkx as nx  # type: ignore
-from sympy import Integer, Expr, Symbol  # type: ignore
+from sympy import Integer, Expr, Symbol, lambdify, plot  # type: ignore
 
 from discopop_explorer.PETGraphX import PETGraphX
 from discopop_library.OptimizationGraph.PETParser.PETParser import PETParser
@@ -17,49 +17,60 @@ class OptimizationGraph(object):
 
     def __init__(self, detection_result):
         self.graph, self.next_free_node_id = PETParser(detection_result.pet).parse()
-        #print("FINAL")
-        #show(self.graph)
+        # print("FINAL")
+        # show(self.graph)
 
         # import parallelization suggestions
         self.graph = import_suggestions(detection_result, self.graph, self.get_next_free_node_id)
-        show(self.graph)
+        # show(self.graph)
 
         function_performance_models = get_performance_models_for_functions(self.graph)
 
-        print_introduced_symbols_per_node(self.graph)
+        # print_introduced_symbols_per_node(self.graph)
 
         print("FUNCTION PERFORMANCE MODELS: ")
         for idx, function in enumerate(function_performance_models):
             for midx, model in enumerate(function_performance_models[function]):
-                print(str(idx)+"-"+str(midx) + ": \t", end="")
+                print(str(idx) + "-" + str(midx) + ": \t", end="")
                 model.print()
+
+        # define variable substitutions
+        substitutions: Dict[Symbol, Expr] = dict()
+        # thread counts
+        substitutions[data_at(self.graph, 105).introduced_symbols[0]] = Integer(4)
+        substitutions[data_at(self.graph, 106).introduced_symbols[0]] = Integer(16)
+
+        # function spawn overhead
+        substitutions[data_at(self.graph, 51).introduced_symbols[0]] = Integer(5)
+        substitutions[data_at(self.graph, 104).introduced_symbols[0]] = Integer(5)
+
+        print("subs: ", substitutions)
+
+        # apply substitutions
+        for idx, function in enumerate(function_performance_models):
+            for midx, model in enumerate(function_performance_models[function]):
+                model.model = model.model.subs(substitutions)
+
+        print("FUNCTION PERFORMANCE MODELS AFTER SUBSTITUTION: ")
+        for idx, function in enumerate(function_performance_models):
+            for midx, model in enumerate(function_performance_models[function]):
+                print(str(idx) + "-" + str(midx) + ": \t", end="")
+                model.print()
+                print(model.model.free_symbols)
+                plot(model.model)
+
 
         print("COMPARE: ")
         for idx_1, function_1 in enumerate(function_performance_models):
             for midx_1, model_1 in enumerate(function_performance_models[function_1]):
                 for idx_2, function_2 in enumerate(function_performance_models):
                     for midx_2, model_2 in enumerate(function_performance_models[function_2]):
-                        print(str(idx_1)+"-"+str(midx_1), str(idx_2)+"-"+str(midx_2), " ==> ", model_1.model.compare(model_2.model))
-
-        # define variable substitutions
-        substitutions: Dict[Symbol, Expr] = dict()
-        substitutions[data_at(self.graph, 105).introduced_symbols[0]] = Integer(42)
-        print("subs: ", substitutions)
-
-        # apply substitutions
-        print_introduced_symbols_per_node(self.graph)
-        print("FUNCTION PERFORMANCE MODELS AFTER SUBSTITUTION: ")
-        for idx, function in enumerate(function_performance_models):
-            for midx, model in enumerate(function_performance_models[function]):
-                print(str(idx) + "-" + str(midx) + ": \t", end="")
-                model.model = model.model.subs(substitutions)
-                model.print()
-
-
-
+                        cmp = model_1.model.compare(model_2.model)
+                        print(str(idx_1) + "-" + str(midx_1), str(idx_2) + "-" + str(midx_2), " ==> ",
+                              "<" if cmp == -1 else "="
+                              if cmp == 0 else ">")
 
     def get_next_free_node_id(self):
         buffer = self.next_free_node_id
         self.next_free_node_id += 1
         return buffer
-
