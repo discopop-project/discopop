fb076770fb7bf3a751553d251909a4f3905f0ab6
Lukas Rothenberger <lukas.rothenberger@tu-darmstadt.de>
2023-04-23 11:51:40 +0200

    insert parallelization suggestions

--- discopop_library/OptimizationGraph/OptimizationGraph.py
+++ discopop_library/OptimizationGraph/OptimizationGraph.py
@@ -3,7 +3,7 @@ import networkx as nx  # type: ignore
 
 from discopop_explorer.PETGraphX import PETGraphX
 from discopop_library.OptimizationGraph.PETParser.PETParser import PETParser
-from discopop_library.OptimizationGraph.PerformanceModels.utilities import get_performance_models_for_functions, \
+from discopop_library.OptimizationGraph.CostModels.utilities import get_performance_models_for_functions, \
     print_introduced_symbols_per_node
 from discopop_library.OptimizationGraph.utilities.MOGUtilities import show
 
@@ -19,6 +19,15 @@ class OptimizationGraph(object):
         print_introduced_symbols_per_node(self.graph)
 
         print("FUNCTION PERFORMANCE MODELS: ")
-        for function in function_performance_models:
+        for idx, function in enumerate(function_performance_models):
+            print(str(idx) + ": \t", end="")
             function_performance_models[function].print()
+
+        print("COMPARE: ")
+        for idx_1, function_1 in enumerate(function_performance_models):
+            for idx_2, function_2 in enumerate(function_performance_models):
+                print(idx_1, idx_2, " ==> ", function_performance_models[function_1].model.compare(function_performance_models[function_2].model))
+
+        # import parallelization suggestions
+        
 
