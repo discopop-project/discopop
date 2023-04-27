a89ead9eacf8fcd15903922e5a50de496a468767
Lukas Rothenberger <lukas.rothenberger@tu-darmstadt.de>
2023-04-27 17:57:42 +0200

    added overhead for do-all suggestions

--- discopop_library/OptimizationGraph/OptimizationGraph.py
+++ discopop_library/OptimizationGraph/OptimizationGraph.py
@@ -15,6 +15,7 @@ from discopop_explorer.PETGraphX import PETGraphX
 from discopop_library.OptimizationGraph.PETParser.PETParser import PETParser
 from discopop_library.OptimizationGraph.CostModels.utilities import get_performance_models_for_functions, \
     print_introduced_symbols_per_node
+from discopop_library.OptimizationGraph.Variables.Environment import Environment
 from discopop_library.OptimizationGraph.suggestions.importers.base import import_suggestions
 from discopop_library.OptimizationGraph.utilities.MOGUtilities import show, data_at
 
@@ -28,8 +29,11 @@ class OptimizationGraph(object):
         # print("FINAL")
         # show(self.graph)
 
+        # define Environment
+        environment = Environment()
+
         # import parallelization suggestions
-        self.graph = import_suggestions(detection_result, self.graph, self.get_next_free_node_id)
+        self.graph = import_suggestions(detection_result, self.graph, self.get_next_free_node_id, environment)
         # show(self.graph)
 
         function_performance_models = get_performance_models_for_functions(self.graph)
@@ -44,14 +48,14 @@ class OptimizationGraph(object):
 
         # define variable substitutions
         substitutions: Dict[Symbol, Expr] = dict()
-        print_introduced_symbols_per_node(self.graph)
+        # print_introduced_symbols_per_node(self.graph)
         # thread counts
-        substitutions[data_at(self.graph, 105).introduced_symbols[0]] = Integer(4)
+        #substitutions[data_at(self.graph, 105).introduced_symbols[0]] = Integer(4)
         #substitutions[data_at(self.graph, 106).introduced_symbols[0]] = Integer(16)
 
         # function spawn overhead
-        substitutions[data_at(self.graph, 51).introduced_symbols[0]] = Integer(5)
-        substitutions[data_at(self.graph, 104).introduced_symbols[0]] = Integer(5)
+        #substitutions[data_at(self.graph, 51).introduced_symbols[0]] = Integer(5)
+        #substitutions[data_at(self.graph, 104).introduced_symbols[0]] = Integer(5)
 
         print("subs: ", substitutions)
 
@@ -66,7 +70,7 @@ class OptimizationGraph(object):
                 print(str(idx) + "-" + str(midx) + ": \t", end="")
                 model.print()
                 print(model.model.free_symbols)
-                plot3d(model.model)
+                plot3d(model.model, (Symbol("transfer_cost"), 0, 500), (Symbol("thread_num"), 1, 1024))
 
 
         print("COMPARE: ")
