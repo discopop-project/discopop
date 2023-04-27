4e68f7143e140f63b47df86a90d732c1f877fd50
Lukas Rothenberger <lukas.rothenberger@tu-darmstadt.de>
2023-04-27 16:06:56 +0200

    added license tags

--- discopop_library/OptimizationGraph/classes/edges/GenericEdge.py
+++ discopop_library/OptimizationGraph/classes/edges/GenericEdge.py
@@ -1,3 +1,10 @@
+# This file is part of the DiscoPoP software (http://www.discopop.tu-darmstadt.de)
+#
+# Copyright (c) 2020, Technische Universitaet Darmstadt, Germany
+#
+# This software may be modified and distributed under the terms of
+# the 3-Clause BSD License.  See the LICENSE file in the package base
+# directory for details.
 from sympy import Integer  # type: ignore
 
 from discopop_library.OptimizationGraph.CostModels.CostModel import CostModel
--- discopop_library/OptimizationGraph/suggestions/importers/base.py
+++ discopop_library/OptimizationGraph/suggestions/importers/base.py
@@ -1,3 +1,10 @@
+# This file is part of the DiscoPoP software (http://www.discopop.tu-darmstadt.de)
+#
+# Copyright (c) 2020, Technische Universitaet Darmstadt, Germany
+#
+# This software may be modified and distributed under the terms of
+# the 3-Clause BSD License.  See the LICENSE file in the package base
+# directory for details.
 import networkx as nx  # type: ignore
 
 from discopop_library.OptimizationGraph.suggestions.importers.do_all import import_suggestion as import_doall
