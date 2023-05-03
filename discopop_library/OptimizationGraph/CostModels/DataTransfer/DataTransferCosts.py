97d33094ead76bd176031bdfecccec7a27986cd0
Lukas Rothenberger <lukas.rothenberger@tu-darmstadt.de>
2023-05-03 16:30:53 +0200

    pass context to successors

---
+++ discopop_library/OptimizationGraph/CostModels/DataTransfer/DataTransferCosts.py
@@ -0,0 +1,12 @@
+from typing import Dict, List, Tuple, Set
+
+import networkx as nx  # type: ignore
+
+from discopop_library.OptimizationGraph.CostModels.CostModel import CostModel
+from discopop_library.OptimizationGraph.classes.context.Update import Update
+from discopop_library.OptimizationGraph.classes.nodes.FunctionRoot import FunctionRoot
+
+
+def add_data_transfer_costs(graph: nx.DiGraph, function_performance_models: Dict[FunctionRoot, List[Tuple[CostModel, Set[Update]]]]):
+    """Calculates the data transfer costs for each of the given performance models and adds them to the respective model."""
+    raise NotImplementedError("TODO")
