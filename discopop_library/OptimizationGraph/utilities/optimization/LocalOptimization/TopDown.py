a4ece922cd1f52c7323d3a085c1b14ad73697748
Lukas Rothenberger <lukas.rothenberger@tu-darmstadt.de>
2023-05-16 17:53:59 +0200

    restrict path decisions

--- discopop_library/OptimizationGraph/utilities/optimization/LocalOptimization/TopDown.py
+++ discopop_library/OptimizationGraph/utilities/optimization/LocalOptimization/TopDown.py
@@ -4,35 +4,50 @@ import networkx as nx  # type: ignore
 from sympy import Symbol, Expr
 
 from discopop_library.OptimizationGraph.CostModels.CostModel import CostModel
+from discopop_library.OptimizationGraph.CostModels.utilities import get_node_performance_models
 from discopop_library.OptimizationGraph.classes.context.ContextObject import ContextObject
 from discopop_library.OptimizationGraph.classes.nodes.FunctionRoot import FunctionRoot
 from discopop_library.OptimizationGraph.utilities.MOGUtilities import get_all_function_nodes, get_successors, \
-    get_children, data_at
+    get_children
 
 
-def get_locally_optimized_models(graph: nx.DiGraph, substitutions: Dict[Symbol, Expr]) -> Dict[FunctionRoot, List[Tuple[CostModel, ContextObject]]]:
+def get_locally_optimized_models(graph: nx.DiGraph, substitutions: Dict[Symbol, Expr]) -> Dict[
+    FunctionRoot, List[Tuple[CostModel, ContextObject]]]:
     result_dict: Dict[FunctionRoot, List[Tuple[CostModel, ContextObject]]] = dict()
     for function_node in get_all_function_nodes(graph):
         # get a list of all decisions that have to be made
         decisions_to_be_made = __find_decisions(graph, function_node)
-#        print("DECISIONS: ", decisions_to_be_made)
-#        for decision in decisions_to_be_made:
-#            print("Decision: ", decision)
-#            for entry in decision:
-#                print("\tEntry: ")
-#                print(data_at(graph, entry).suggestion)
 
+        # create performance models for individual options of decisions
+        for decision_options in decisions_to_be_made:
+            print("Decision options: ", decision_options)
+            for decision in decision_options:
+                print("DECISION: ", decision)
+                # create a performance model for the specific decision
+                print("\trestrict to: ", {decision})
+                print("\tdo not allow: ", set(
+                                                                     [o for o in decision_options if o != decision]))
+                performance_models = get_node_performance_models(graph, function_node, set(),
+                                                                 restrict_to_decisions={decision},
+                                                                 do_not_allow_decisions=set(
+                                                                     [o for o in decision_options if o != decision]))
+                print("PERFORMANCE MODELS: ")
+                for model in performance_models:
+                    print(model.path_decisions)
 
-#        def get_node_performance_models(
-#                graph: nx.DiGraph, node_id: int, visited_nodes: Set[int],
-#                restrict_to_decisions: Optional[Set[int]] = None
-#
-#
-#            locally_optimal_choices: List[int] = []
+                #            for entry in decision:
+                #                print("\tEntry: ")
+                #                print(data_at(graph, entry).suggestion)
 
+                #        def get_node_performance_models(
+                #                graph: nx.DiGraph, node_id: int, visited_nodes: Set[int],
+                #                restrict_to_decisions: Optional[Set[int]] = None
+                #
+                #
+                #            locally_optimal_choices: List[int] = []
 
-    import sys
-    sys.exit(0)
+                import sys
+                sys.exit(0)
 
     return result_dict
 
