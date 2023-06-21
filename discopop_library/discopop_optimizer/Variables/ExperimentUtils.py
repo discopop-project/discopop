448ada1dddb5af47dca0106ba428babfc12dc3be
Lukas Rothenberger <lukas.rothenberger@tu-darmstadt.de>
2023-06-21 14:28:17 +0200

    cleanup
    cleanup

--- discopop_library/discopop_optimizer/Variables/ExperimentUtils.py
+++ discopop_library/discopop_optimizer/Variables/ExperimentUtils.py
@@ -20,10 +20,7 @@ def show_function_models(
         show_functions if show_functions is not None else experiment.function_models
     )
     for function in considered_functions:
-        print("FUNCTION: ", function)
-        print("\ttype: ", type(function))
         print("Showing function models: ", function.name)
-        print("IN OPTIONS: ", len(experiment.function_models[function]))
         options = show_options(  # random options might be added by show_options
             experiment.detection_result.pet,
             experiment.optimization_graph,
@@ -36,7 +33,6 @@ def show_function_models(
             function,
             window_title="Function: " + function.name,
         )
-        print("OUT OPTIONS: ", len(options))
 
 
 def export_to_json(experiment: Experiment):
@@ -44,12 +40,7 @@ def export_to_json(experiment: Experiment):
     to_be_added = []
     to_be_deleted = []
     for old_key in experiment.function_models:
-        print("OLD KEY: ", old_key)
-        print("NODE_ID: ", old_key.node_id)
-        print("Tyoe: ", type(old_key))
         new_key = old_key.node_id
-        print("--> New key: ", new_key)
-        print("--> Type: ", type(new_key))
         to_be_added.append((new_key, experiment.function_models[old_key]))
         to_be_deleted.append(old_key)
 
@@ -58,35 +49,21 @@ def export_to_json(experiment: Experiment):
     for k2, v in to_be_added:
         experiment.function_models[k2] = v  # type: ignore
 
-    print("FUNCTION MODELS: ")
-    print(experiment.function_models)
-
     experiment_dump_path: str = os.path.join(
         experiment.discopop_optimizer_path, "last_experiment.pickle"
     )
     if not os.path.exists(experiment.discopop_optimizer_path):
         os.makedirs(experiment.discopop_optimizer_path)
-    # with open(experiment_dump_path, "w+") as f:
-    #    f.write(jsonpickle.encode(experiment))
-    #    print("Saved experiment to: ", experiment_dump_path)
     pickle.dump(experiment, open(experiment_dump_path, "wb"))
 
 
 def restore_session(json_file: str) -> Experiment:
-    # with open(json_file, "r") as f:
-    #    experiment = jsonpickle.decode(f.read())
     experiment = pickle.load(open(json_file, "rb"))
 
     # convert keys of function_models to FunctionRoot objects
     to_be_added = []
     to_be_deleted = []
     for old_key in experiment.function_models:
-        print("TYPE: ", type(old_key))
-        print("GRAPH NODES: ")
-        print(experiment.optimization_graph.nodes)
-        for node in experiment.optimization_graph.nodes:
-            print("Node: ", node)
-            print("\ttype: ", type(data_at(experiment.optimization_graph, node)))
         new_key = cast(FunctionRoot, data_at(experiment.optimization_graph, old_key))
         to_be_added.append((new_key, experiment.function_models[old_key]))
         to_be_deleted.append(old_key)
