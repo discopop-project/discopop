45f630e7ce13cecbff6fd98206bfe6903f158c3c
Lukas Rothenberger <lukas.rothenberger@tu-darmstadt.de>
2023-06-28 11:54:18 +0200

    feat(discopop_wizard): execute configurations marked by a specific tag

--- discopop_wizard/classes/Arguments.py
+++ discopop_wizard/classes/Arguments.py
@@ -5,6 +5,11 @@
 # This software may be modified and distributed under the terms of
 # the 3-Clause BSD License.  See the LICENSE file in the package base
 # directory for details.
+from typing import List
+
 
 class Arguments(object):
     no_gui: bool = False
+    execute_all_configurations: bool = False
+    execute_configurations_with_tag: List[str] = []
+
