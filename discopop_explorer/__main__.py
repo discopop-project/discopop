e36cf9e072a056a923efd14bddd1390e510348d6
goerlibe <goerlibe@noreply.github.com>
2023-07-16 13:33:06 +0200

    refactor: move run method out of __init__.py

--- discopop_explorer/__main__.py
+++ discopop_explorer/__main__.py
@@ -52,8 +52,13 @@ import time
 from docopt import docopt  # type:ignore
 from schema import Schema, Use, SchemaError  # type:ignore
 from pathlib import Path
+from pluginbase import PluginBase  # type:ignore
+from typing import List, Optional
 
-from . import run, __version__
+from ._version import __version__
+from .parser import parse_inputs
+from .PETGraphX import PETGraphX
+from .pattern_detection import DetectionResult, PatternDetectorX
 from .json_serializer import PatternInfoSerializer
 
 docopt_schema = Schema(
@@ -86,6 +91,53 @@ def get_path(base_path: str, file_name: str) -> str:
     :return: path to file
     """
     return file_name if os.path.isabs(file_name) else os.path.join(base_path, file_name)
+
+def run(
+    cu_xml: str,
+    dep_file: str,
+    loop_counter_file: str,  # TODO we should be able to read all info from the _dep.txt file (?)
+    reduction_file: str,
+    plugins: List[str],
+    file_mapping: Optional[str] = None,
+    cu_inst_result_file: Optional[str] = None,
+    llvm_cxxfilt_path: Optional[str] = None,
+    discopop_build_path: Optional[str] = None,
+    enable_task_pattern: bool = False,
+) -> DetectionResult:
+    pet = PETGraphX.from_parsed_input(*parse_inputs(cu_xml, dep_file, reduction_file, file_mapping))
+    print("PET CREATION FINISHED.")
+    # pet.show()
+    # TODO add visualization
+
+    plugin_base = PluginBase(package="plugins")
+
+    plugin_source = plugin_base.make_plugin_source(searchpath=[Path(__file__).parent / "plugins"])
+
+    for plugin_name in plugins:
+        p = plugin_source.load_plugin(plugin_name)
+        print("executing plugin before: " + plugin_name)
+        pet = p.run_before(pet)
+
+    pattern_detector = PatternDetectorX(pet)
+
+    res: DetectionResult = pattern_detector.detect_patterns(
+        cu_xml,
+        dep_file,
+        loop_counter_file,
+        reduction_file,
+        file_mapping,
+        cu_inst_result_file,
+        llvm_cxxfilt_path,
+        discopop_build_path,
+        enable_task_pattern,
+    )
+
+    for plugin_name in plugins:
+        p = plugin_source.load_plugin(plugin_name)
+        # print("executing plugin after: " + plugin_name)
+        pet = p.run_after(pet)
+
+    return res
 
 
 def main():
