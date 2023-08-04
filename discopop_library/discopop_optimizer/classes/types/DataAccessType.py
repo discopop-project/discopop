270324ab0226742da46447aa6b54bab1e874f6df
Lukas Rothenberger <lukas.rothenberger@tu-darmstadt.de>
2023-08-04 11:23:25 +0200

    added license tags

--- discopop_library/discopop_optimizer/classes/types/DataAccessType.py
+++ discopop_library/discopop_optimizer/classes/types/DataAccessType.py
@@ -1,3 +1,10 @@
+# This file is part of the DiscoPoP software (http://www.discopop.tu-darmstadt.de)
+#
+# Copyright (c) 2020, Technische Universitaet Darmstadt, Germany
+#
+# This software may be modified and distributed under the terms of
+# the 3-Clause BSD License.  See the LICENSE file in the package base
+# directory for details.
 from typing import Tuple, Optional
 
 from discopop_explorer.PETGraphX import MemoryRegion
