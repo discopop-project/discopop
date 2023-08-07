dd52d1fb003a4508247e1204dfb241a343768ff8
Lukas Rothenberger <lukas.rothenberger@tu-darmstadt.de>
2023-08-07 17:09:06 +0200

    Added CMAKE wrapper + minor fixes

--- scripts/CC_wrapper.sh
+++ scripts/CC_wrapper.sh
@@ -3,11 +3,19 @@ DP_DIR=/home/lukas/git/discopop
 DP_BUILD=${DP_DIR}/build
 DP_SCRIPTS=${DP_DIR}/scripts
 
-
 # original arguments: "$@"
 echo "WRAPPED CC COMPILE..."
 echo "ARGS: ${@}"
-echo "clang-11 -g -c -O0 -S -emit-llvm -fno-discard-value-names ${@} -Xclang -load -Xclang ${DP_BUILD}/libi/LLVMDiscoPoP.so -DiscoPoP -mllvm --fm-path -mllvm ${DISCOPOP_FILEMAPPING_PATH}"
-clang-11 -g -c -O0 -S -emit-llvm -fno-discard-value-names "$@" -Xclang -load -Xclang ${DP_BUILD}/libi/LLVMDiscoPoP.so -DiscoPoP -mllvm --fm-path -mllvm ${DISCOPOP_FILEMAPPING_PATH}
+echo "DP_FM_PATH: ${DP_FM_PATH}"
+
+# check if environment is prepared
+if [ -z ${DP_FM_PATH} ]; then
+  echo "ERROR: DP_FM_PATH unspecified!"
+  echo "\tGenerate a FileMapping.txt file and create an environment Variable which points to this file."
+  exit 1
+fi
+
+echo "clang-11 -g -c -O0 -S -emit-llvm -fno-discard-value-names ${@} -Xclang -load -Xclang ${DP_BUILD}/libi/LLVMDiscoPoP.so -DiscoPoP -mllvm --fm-path -mllvm ${DP_FM_PATH}"
+clang-11 -g -c -O0 -S -emit-llvm -fno-discard-value-names "$@" -Xclang -load -Xclang ${DP_BUILD}/libi/LLVMDiscoPoP.so -DiscoPoP -mllvm --fm-path -mllvm ${DP_FM_PATH}
 
 # WARNING: OUTPUT IS A .ll FILE, ENDING IS .o
