c38a2c2cdf711ceead8ee594a67af1c89ee63780
Lukas Rothenberger <lukas.rothenberger@tu-darmstadt.de>
2023-08-21 10:52:41 +0200

    added check for location of invoked script

--- scripts/LINKER_wrapper.sh
+++ scripts/LINKER_wrapper.sh
@@ -8,8 +8,19 @@
 # the 3-Clause BSD License. See the LICENSE file in the package base
 # directory for details.
 
+SCRIPT_PATH="$(readlink -fm "$0")"
+
+# ensure that copy of script in build folder is invoked instead of the "original"
+if [[ "${SCRIPT_PATH}" == *"discopop/scripts/"* ]]; then
+  echo "ERROR: Invoked script directly from source folder:"
+  echo "    ${SCRIPT_PATH}"
+  echo "Use the copy located in the DiscoPoP build folder instead."
+  echo ""
+  exit 1
+fi
+
 # SETTINGS
-DP_BUILD="$(dirname "$(dirname "$(readlink -fm "$0")")")"
+DP_BUILD="$(dirname "$(dirname ${SCRIPT_PATH})")"
 DP_SCRIPTS=${DP_BUILD}/scripts
 
 
