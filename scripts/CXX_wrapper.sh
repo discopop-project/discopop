d4829131f16f8385b60c70b10720a8703d3fb498
Lukas Rothenberger <lukas.rothenberger@tu-darmstadt.de>
2023-08-29 13:29:26 +0200

    feature(end-to-end tests): first draft .

--- scripts/CXX_wrapper.sh
+++ scripts/CXX_wrapper.sh
@@ -39,7 +39,7 @@ if [ -z ${DP_FM_PATH} ]; then
   exit 1
 fi
 
-echo "${LLVM_CLANGPP} -g -c -O0 -S -emit-llvm -fno-discard-value-names ${@} -Xclang -load -Xclang ${DP_BUILD}/libi/LLVMDiscoPoP.so -DiscoPoP -mllvm --fm-path -mllvm ${DP_FM_PATH}"
+echo "${LLVM_CLANGPP} -g -c -O0 -S -emit-llvm -fno-discard-value-names ${@} -Xclang -load -Xclang ${DP_BUILD}/libi/LLVMDiscoPoP.so -DiscoPoP -mllvm --fm-path -mllvm ${DP_FM_PATH} "
 #clang++-11 -g -c -O0 -S -emit-llvm -fno-discard-value-names "$@" -Xclang -load -Xclang ${DP_BUILD}/libi/LLVMDiscoPoP.so -DiscoPoP -mllvm --fm-path -mllvm ${DP_FM_PATH}
 ${LLVM_CLANGPP} "$@" -g -c -O0 -fno-discard-value-names -Xclang -load -Xclang ${DP_BUILD}/libi/LLVMDiscoPoP.so -DiscoPoP -mllvm --fm-path -mllvm ${DP_FM_PATH} -fPIC
 
---
+++ test/do_all/daxpy/test.py
@@ -0,0 +1,57 @@
+import os
+import unittest
+import pathlib
+import jsonpickle
+
+from discopop_explorer.test.utils.validators import check_do_all_equivalence
+from discopop_library.result_classes.DetectionResult import DetectionResult
+
+
+class TestMethods(unittest.TestCase):
+    def test(self):
+        current_dir = pathlib.Path(__file__).parent.resolve()
+        discopop_dir = os.path.join(current_dir, "..", "..", "..")
+        build_dir = os.path.join(discopop_dir, "build")
+
+        src_dir = os.path.join(current_dir, "src")
+        os.chdir(src_dir)
+
+        # create FileMapping
+        os.system("" + os.path.join(build_dir, "scripts", "dp-fmap"))
+
+        # build
+        make_command = "DP_FM_PATH=" + os.path.join(src_dir, "FileMapping.txt") + " "
+        make_command += "CC=" + os.path.join(build_dir, "scripts", "CC_wrapper.sh") + " "
+        make_command += "CXX=" + os.path.join(build_dir, "scripts", "CXX_wrapper.sh") + " "
+        make_command += "LINKER=" + os.path.join(build_dir, "scripts", "LINKER_wrapper.sh") + " "
+        make_command += "make "
+        os.system(make_command)
+        # execute instrumented program
+        os.system("./prog")
+        # execute DiscoPoP analysis
+        os.system("discopop_explorer --dep-file=prog_dep.txt")
+        # validate results
+        validation_result = validate_results(current_dir, src_dir)
+        # clean environment
+        os.system("make veryclean")
+
+        assert validation_result
+
+
+def validate_results(test_dir, src_dir) -> bool:
+    """compare results to gold standard"""
+    gold_standard_file = os.path.join(test_dir, "detection_result_dump.json")
+    test_output_file = os.path.join(src_dir, "detection_result_dump.json")
+    # load both detection results
+    tmp_str = ""
+    with open(gold_standard_file, "r") as f:
+        tmp_str = f.read()
+    gold_standard: DetectionResult = jsonpickle.decode(tmp_str)
+
+    tmp_str = ""
+    with open(test_output_file, "r") as f:
+        tmp_str = f.read()
+    test_output: DetectionResult = jsonpickle.decode(tmp_str)
+
+    # check both ways to ensure exactly the correct patterns have been identified
+    return check_do_all_equivalence(gold_standard.do_all, test_output.do_all)
