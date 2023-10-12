d7d49ee6e241b831db33ef010598840a60bbeeb5
samhm1 <128360761+samhm1@users.noreply.github.com>
2023-10-12 14:58:29 +0200

    Delete test directory

--- hotspot_detection/test/run.sh
+++
@@ -1,8 +0,0 @@
-$HOME/.../clang-8 -S -emit-llvm rtlib.c
-$HOME/.../clang-8 -g -O0 -fno-discard-value-names -Xclang -load -Xclang $HOME/.../HDPass/build/skeleton/libSkeletonPass.so -S -emit-llvm mycode.c
-$HOME/.../llvm-as rtlib.ll -o rtlib.bc
-$HOME/.../llvm-as mycode.ll -o mycode.bc
-$HOME/.../llvm-link mycode.bc rtlib.bc -o a.bc
-$HOME/.../clang-8 a.bc -o a.out
-
-#delete the temp.txt before bash ./run.sh
