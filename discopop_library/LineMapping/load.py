2fe89af21b851f5d5ce1aedda95b7b93ac5635b7
Lukas Rothenberger <lukas.rothenberger@tu-darmstadt.de>
2023-11-01 14:10:04 +0100

    feat(line_mapping): utilities to save and load added

---
+++ discopop_library/LineMapping/load.py
@@ -0,0 +1,18 @@
+# This file is part of the DiscoPoP software (http://www.discopop.tu-darmstadt.de)
+#
+# Copyright (c) 2020, Technische Universitaet Darmstadt, Germany
+#
+# This software may be modified and distributed under the terms of
+# the 3-Clause BSD License.  See the LICENSE file in the package base
+# directory for details.
+import json
+import os.path
+from typing import Dict
+
+
+def load_line_mapping(discopop_path: str = ".discopop") -> Dict[int, Dict[int, int]]:
+    """Loads and returns the line_mapping dictionary"""
+    line_mapping_dict: Dict[int, Dict[int, int]] = dict()
+    with open(os.path.join(discopop_path, "line_mapping.json")) as f:
+        line_mapping_dict = json.load(f)
+    return line_mapping_dict
