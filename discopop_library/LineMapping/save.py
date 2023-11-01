447761a55e6903b78b451103f596c9899822d917
Lukas Rothenberger <lukas.rothenberger@tu-darmstadt.de>
2023-11-01 14:10:04 +0100

    feat(line_mapping): utilities to save and load added

---
+++ discopop_library/LineMapping/save.py
@@ -0,0 +1,17 @@
+# This file is part of the DiscoPoP software (http://www.discopop.tu-darmstadt.de)
+#
+# Copyright (c) 2020, Technische Universitaet Darmstadt, Germany
+#
+# This software may be modified and distributed under the terms of
+# the 3-Clause BSD License.  See the LICENSE file in the package base
+# directory for details.
+import json
+import os
+from typing import Dict
+
+
+def save_line_mapping(line_mapping_dict: Dict[int, Dict[int, int]], discopop_path: str = ".discopop"):
+    """dumps line_mapping_dict to line_mapping.json"""
+    # dump line mapping to json file
+    with open(os.path.join(discopop_path, "line_mapping.json"), "w+") as f:
+        json.dump(line_mapping_dict, f)
