81ef00e72b629a8c6f75114be0c2a2e79be635e1
Lukas Rothenberger <lukas.rothenberger@tu-darmstadt.de>
2023-11-06 10:21:23 +0100

    feat(patch_generator): read CC /CXX from build_config.txt

--- discopop_library/PatchGenerator/__main__.py
+++ discopop_library/PatchGenerator/__main__.py
@@ -5,8 +5,9 @@
 # This software may be modified and distributed under the terms of
 # the 3-Clause BSD License.  See the LICENSE file in the package base
 # directory for details.
-
+import os.path
 from argparse import ArgumentParser
+from pathlib import Path
 
 from discopop_library.PatchGenerator.PatchGeneratorArguments import PatchGeneratorArguments
 from discopop_library.PatchGenerator.patch_generator import run
@@ -24,12 +25,42 @@ def parse_args() -> PatchGeneratorArguments:
     # fmt: off
     parser.add_argument("--verbose", action="store_true",
         help="Enable verbose output.")
+    parser.add_argument(
+        "--dp-build-path", type=str, required=True,
+        help="Path to DiscoPoP build folder"
+    )
     # EXPERIMENTAL FLAGS:
     # fmt: on
 
     arguments = parser.parse_args()
 
-    return PatchGeneratorArguments(verbose=arguments.verbose)
+    # determine CC and CXX
+    with open(os.path.join(arguments.dp_build_path, "build_config.txt"), "r") as f:
+        for line in f.readlines():
+            if line.startswith("LLVM_BIN_DIR="):
+                line = line.replace("\n", "")
+                llvm_bin_dir = line[len("LLVM_BIN_DIR=") :]
+                # determine CC
+                if os.path.exists(os.path.join(llvm_bin_dir, "clang")):
+                    arguments.cc = os.path.join(llvm_bin_dir, "clang")
+                elif os.path.exists(os.path.join(llvm_bin_dir, "clang-11")):
+                    arguments.cc = os.path.join(llvm_bin_dir, "clang-11")
+                else:
+                    raise ValueError("Could not determine CC from discopop build path: ", arguments.dp_build_path)
+
+                # determine CXX
+                if os.path.exists(os.path.join(llvm_bin_dir, "clang++")):
+                    arguments.cxx = os.path.join(llvm_bin_dir, "clang++")
+                elif os.path.exists(os.path.join(llvm_bin_dir, "clang++-11")):
+                    arguments.cxx = os.path.join(llvm_bin_dir, "clang++-11")
+                else:
+                    raise ValueError("Could not determine CXX from discopop build path: ", arguments.dp_build_path)
+
+                break
+
+    return PatchGeneratorArguments(
+        verbose=arguments.verbose, discopop_build_path=arguments.dp_build_path, CC=arguments.cc, CXX=arguments.cxx
+    )
 
 
 def main():
