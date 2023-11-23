59744195e359bc1728ca1bbdde7c54eef81c432c
Lukas Rothenberger <lukas.rothenberger@tu-darmstadt.de>
2023-11-23 11:04:26 +0100

    feat(patch_generator): use config_provider instead of  argument

--- discopop_library/PatchGenerator/__main__.py
+++ discopop_library/PatchGenerator/__main__.py
@@ -11,6 +11,8 @@ from pathlib import Path
 
 from discopop_library.PatchGenerator.PatchGeneratorArguments import PatchGeneratorArguments
 from discopop_library.PatchGenerator.patch_generator import run
+from discopop_library.ConfigProvider.config_provider import run as run_config_provider
+from discopop_library.ConfigProvider.ConfigProviderArguments import ConfigProviderArguments
 
 
 def parse_args() -> PatchGeneratorArguments:
@@ -19,44 +21,41 @@ def parse_args() -> PatchGeneratorArguments:
     # all flags that are not considered stable should be added to the experimental_parser
     experimental_parser = parser.add_argument_group(
         "EXPERIMENTAL",
-        "Arguments for the task pattern detector and other experimental features. These flags are considered EXPERIMENTAL and they may or may not be removed or changed in the near future.",
+        "Arguments marked as experimental features. These flags may or may not be removed or changed in the future.",
     )
 
     # fmt: off
     parser.add_argument("--verbose", action="store_true",
         help="Enable verbose output.")
-    parser.add_argument(
-        "--dp-build-path", type=str, required=True,
-        help="Path to DiscoPoP build folder"
-    )
     # EXPERIMENTAL FLAGS:
     # fmt: on
 
     arguments = parser.parse_args()
 
-    # determine CC and CXX
-    with open(os.path.join(arguments.dp_build_path, "build_config.txt"), "r") as f:
-        for line in f.readlines():
-            if line.startswith("LLVM_BIN_DIR="):
-                line = line.replace("\n", "")
-                llvm_bin_dir = line[len("LLVM_BIN_DIR=") :]
-                # determine CC
-                if os.path.exists(os.path.join(llvm_bin_dir, "clang")):
-                    arguments.cc = os.path.join(llvm_bin_dir, "clang")
-                elif os.path.exists(os.path.join(llvm_bin_dir, "clang-11")):
-                    arguments.cc = os.path.join(llvm_bin_dir, "clang-11")
-                else:
-                    raise ValueError("Could not determine CC from discopop build path: ", arguments.dp_build_path)
+    # determine DP build path
+    arguments.dp_build_path = run_config_provider(
+        ConfigProviderArguments(return_dp_build_dir=True, return_dp_source_dir=False, return_llvm_bin_dir=False)
+    )
 
-                # determine CXX
-                if os.path.exists(os.path.join(llvm_bin_dir, "clang++")):
-                    arguments.cxx = os.path.join(llvm_bin_dir, "clang++")
-                elif os.path.exists(os.path.join(llvm_bin_dir, "clang++-11")):
-                    arguments.cxx = os.path.join(llvm_bin_dir, "clang++-11")
-                else:
-                    raise ValueError("Could not determine CXX from discopop build path: ", arguments.dp_build_path)
+    # determine LLVM_BIN_DIR
+    llvm_bin_dir = run_config_provider(
+        ConfigProviderArguments(return_dp_build_dir=False, return_dp_source_dir=False, return_llvm_bin_dir=True)
+    )
+    # determine CC
+    if os.path.exists(os.path.join(llvm_bin_dir, "clang")):
+        arguments.cc = os.path.join(llvm_bin_dir, "clang")
+    elif os.path.exists(os.path.join(llvm_bin_dir, "clang-11")):
+        arguments.cc = os.path.join(llvm_bin_dir, "clang-11")
+    else:
+        raise ValueError("Could not determine CC from LLVM_BIN_DIR: ", llvm_bin_dir)
 
-                break
+    # determine CXX
+    if os.path.exists(os.path.join(llvm_bin_dir, "clang++")):
+        arguments.cxx = os.path.join(llvm_bin_dir, "clang++")
+    elif os.path.exists(os.path.join(llvm_bin_dir, "clang++-11")):
+        arguments.cxx = os.path.join(llvm_bin_dir, "clang++-11")
+    else:
+        raise ValueError("Could not determine CXX from LLVM_BIN_DIR: ", llvm_bin_dir)
 
     return PatchGeneratorArguments(
         verbose=arguments.verbose, discopop_build_path=arguments.dp_build_path, CC=arguments.cc, CXX=arguments.cxx
