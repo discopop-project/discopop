e031e7b5336aeaa5acdf1cc79f4d5a62a26f4c8d
Lukas Rothenberger <lukas.rothenberger@tu-darmstadt.de>
2023-12-13 17:23:32 +0100

    chore(library): cleanup

--- discopop_library/ParallelConfiguration/ParallelConfiguration.py
+++ discopop_library/ParallelConfiguration/ParallelConfiguration.py
@@ -29,7 +29,6 @@ class ParallelConfiguration(object):
         self.data_movement = []
         self.pattern_id = None
 
-
     def reconstruct_from_file(self, file_path: str):
         with open(file_path, "r") as f:
             loaded_data = json.load(f)
@@ -40,7 +39,7 @@ class ParallelConfiguration(object):
             self.data_movement.append(construct_update_from_dict(values))
 
     def dump_to_file(self, file_path: str):
-        dumpable_dict = dict()
+        dumpable_dict: Dict[str, Any] = dict()
         dumpable_dict["applied_patterns"] = self.applied_patterns
         dumpable_dict["data_movement"] = [update.toDict() for update in self.data_movement]
         dumpable_dict["pattern_id"] = self.pattern_id
@@ -54,7 +53,7 @@ class ParallelConfiguration(object):
     def add_pattern(self, pattern_id: SuggestionId, target_device_id: DeviceID, device_type: DeviceTypeEnum):
         if self.pattern_id is None:
             # get a unique pattern_id
-            with open("next_free_pattern_id.txt", "r") as f: 
+            with open("next_free_pattern_id.txt", "r") as f:
                 self.pattern_id = int(f.read())
             with open("next_free_pattern_id.txt", "w") as f:
                 f.write(str(self.pattern_id + 1))
