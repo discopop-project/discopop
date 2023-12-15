9dfb5611a4c3c1da2dd21f16ee914a3df42bec78
Lukas Rothenberger <lukas.rothenberger@tu-darmstadt.de>
2023-12-15 12:54:14 +0100

    fix(library): added license tags

--- discopop_library/discopop_optimizer/UpdateOptimization/LoopInitializationUpdates.py
+++ discopop_library/discopop_optimizer/UpdateOptimization/LoopInitializationUpdates.py
@@ -1,3 +1,11 @@
+# This file is part of the DiscoPoP software (http://www.discopop.tu-darmstadt.de)
+#
+# Copyright (c) 2020, Technische Universitaet Darmstadt, Germany
+#
+# This software may be modified and distributed under the terms of
+# the 3-Clause BSD License.  See the LICENSE file in the package base
+# directory for details.
+
 from discopop_library.ParallelConfiguration.ParallelConfiguration import ParallelConfiguration
 from discopop_library.discopop_optimizer.OptimizerArguments import OptimizerArguments
 from discopop_library.discopop_optimizer.Variables.Experiment import Experiment
