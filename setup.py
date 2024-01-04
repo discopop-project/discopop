324e897f59fa493a06ffb5830e0a3fc6dbaeed0c
Lukas Rothenberger <lukas.rothenberger@tu-darmstadt.de>
2024-01-04 12:48:40 +0100

    cleanup: mark discopop_profiler and _wizard as deprecated

--- setup.py
+++ setup.py
@@ -56,8 +56,8 @@ setup(
     entry_points={
         "console_scripts": [
             "discopop_explorer=discopop_explorer.__main__:main",
-            "discopop_profiler=discopop_profiler.__main__:main",
-            "discopop_wizard=discopop_wizard.__main__:main",
+            "DEPRECATED_discopop_profiler=DEPRECATED_discopop_profiler.__main__:main",
+            "DEPRECATED_discopop_wizard=DEPRECATED_discopop_wizard.__main__:main",
             "discopop_code_generator=discopop_library.CodeGenerator.__main__:main",
             "discopop_optimizer=discopop_library.discopop_optimizer.__main__:main",
             "discopop_patch_generator=discopop_library.PatchGenerator.__main__:main",
