cdec49c053115db72d9c581711b7fdbd2210c71f
Lukas Rothenberger <lukas.rothenberger@tu-darmstadt.de>
2024-01-17 09:34:06 +0100

    cleanup and parallelization

--- discopop_library/discopop_optimizer/optimization/greedy.py
+++ discopop_library/discopop_optimizer/optimization/greedy.py
@@ -45,12 +45,12 @@ def greedy_search(
     global_arguments = arguments
 
     # identify sequential suggestions
-    # copy available decisions
+    # copy available decisions and convert function nodes to node ids
     sequential_suggestions = dict()
     for key in available_decisions:
-        sequential_suggestions[key] = []
+        sequential_suggestions[key.node_id] = []
         for entry in available_decisions[key]:
-            sequential_suggestions[key].append(copy.deepcopy(entry))
+            sequential_suggestions[key.node_id].append(copy.deepcopy(entry))
 
     for function in sequential_suggestions:
         for decision_set in sequential_suggestions[function]:
@@ -73,18 +73,14 @@ def greedy_search(
         for entry in sequential_suggestions[key]:
             made_decisions[key].append(copy.deepcopy(entry))
     
-    
-    def get_dicision_list(decisions_dict):
-        res_list = []
-        for function in decisions_dict:
-            for decision_set in decisions_dict[function]:
-                res_list.append(decision_set[0])
-        return res_list
 
     for idx, function in enumerate(available_decisions):
         print("Greedy searching function: ", function.name, idx, "/", len(available_decisions))
         for dcsi, decision_set in enumerate(available_decisions[function]):
-            local_results: List[Tuple[Dict[Any, Any], Expr]] = []
+            local_results: List[Tuple[Dict[Any, Any], int]] = []
+            
+            # prepare arguments for parallel cost calculation
+            param_list = []
             for decision in decision_set:
                 # copy made decisions
                 local_decision_set = dict()
@@ -93,20 +89,68 @@ def greedy_search(
                     for entry in made_decisions[key]:
                         local_decision_set[key].append(copy.deepcopy(entry))
                 
-                local_decision_set[function][dcsi] = [decision]
+                local_decision_set[function.node_id][dcsi] = [decision]
+                param_list.append(local_decision_set)
 
-                _, score_expr, context = evaluate_configuration(
-                    cast(Experiment, global_experiment),
-                    get_dicision_list(local_decision_set),
-                    cast(OptimizerArguments, global_arguments),
-                )
-                local_results.append((local_decision_set, score_expr))
+            # calculate costs in parallel
+            with Pool(initializer=__initialize_cost_caluclation_worker, initargs=(experiment, arguments)) as pool:
+                tmp_result = list(tqdm.tqdm(pool.imap_unordered(__get_score, param_list), total=len(param_list), disable=True))
+            for local_result in tmp_result:
+                # remove invalid elements
+                if local_result[1] == -1:
+                    continue                
+                local_results.append(local_result)
+
             
             # identify best option and update made_decisions
-            made_decisions = sorted(local_results, key=lambda x: x[1])[0][0]
-            print("Selection: ", get_dicision_list(made_decisions))
+            best_option: Tuple[Dict[Any, Any], int] = None
+            for k, e in local_results:
+                if best_option is None:
+                    best_option = (k, e)
+                    continue
+                if e < best_option[1]:
+                    best_option = (k, e)
             
-
+            made_decisions = best_option[0]
+            
+#            made_decisions = sorted(local_results, key=lambda x: x[1])[0][0]
+            print("Selection: ", __get_dicision_list(made_decisions), "costs:", best_option[1])
+            
 
     import sys
     sys.exit(0)
+
+def __get_dicision_list(decisions_dict):
+    """Converts a dictionary based description of a configuration into a list of integers"""
+    res_list = []
+    for function in decisions_dict:
+        for decision_set in decisions_dict[function]:
+            res_list.append(decision_set[0])
+    return res_list
+
+
+def __initialize_cost_caluclation_worker(
+    experiment: Experiment,
+    arguments: OptimizerArguments,
+):
+    global global_experiment
+    global global_arguments
+    global_experiment = experiment
+    global_arguments = arguments
+
+
+def __get_score(param_tuple) -> Tuple[List[int], int, ContextObject]:
+    global global_experiment
+    global global_arguments
+    configuration = param_tuple
+    try:
+        _, score_expr, _ = evaluate_configuration(
+            cast(Experiment, global_experiment),
+            __get_dicision_list(configuration),
+            cast(OptimizerArguments, global_arguments),
+        )
+        result = int(float(str(score_expr.evalf())))
+    except ValueError:
+        result = -1
+
+    return configuration, result
