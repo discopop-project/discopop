a68081cd101d114ff33330adb869116ee008c46a
goerlibe <23436477+goerlibe@users.noreply.github.com>
2024-01-19 12:16:09 +0100

    fix(config_provider): return 0, remove unneccesary casts, mutually exclusive arguments

--- discopop_library/ConfigProvider/config_provider.py
+++ discopop_library/ConfigProvider/config_provider.py
@@ -6,7 +6,6 @@
 # the 3-Clause BSD License.  See the LICENSE file in the package base
 # directory for details.
 
-from typing import cast
 from discopop_library.ConfigProvider.ConfigProviderArguments import ConfigProviderArguments
 from discopop_library.ConfigProvider.assets.build_config import DP_BUILD, DP_SOURCE, LLVM_BIN_DIR  # type: ignore
 
@@ -15,10 +14,10 @@ def run(arguments: ConfigProviderArguments) -> str:
     """Returns the contents of the written build_config.txt"""
 
     if arguments.return_dp_build_dir:
-        return cast(str, DP_BUILD)  # type: ignore
+        return DP_BUILD  # type: ignore
     elif arguments.return_dp_source_dir:
-        return cast(str, DP_SOURCE)  # type: ignore
+        return DP_SOURCE  # type: ignore
     elif arguments.return_llvm_bin_dir:
-        return cast(str, LLVM_BIN_DIR)  # type: ignore
+        return LLVM_BIN_DIR  # type: ignore
     else:
         raise ValueError("No valid operation for execution configuration: \n" + str(arguments))
