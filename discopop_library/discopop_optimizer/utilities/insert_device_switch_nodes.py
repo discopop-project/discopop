33cd01c721f2729b29c39698de8cdddf1302a2e9
Lukas Rothenberger <lukas.rothenberger@tu-darmstadt.de>
2024-01-26 17:04:53 +0100

    fix: add license tags

--- discopop_library/discopop_optimizer/utilities/insert_device_switch_nodes.py
+++ discopop_library/discopop_optimizer/utilities/insert_device_switch_nodes.py
@@ -1,3 +1,11 @@
+# This file is part of the DiscoPoP software (http://www.discopop.tu-darmstadt.de)
+#
+# Copyright (c) 2020, Technische Universitaet Darmstadt, Germany
+#
+# This software may be modified and distributed under the terms of
+# the 3-Clause BSD License.  See the LICENSE file in the package base
+# directory for details.
+
 from typing import List, Set, Tuple, cast
 import networkx as nx  # type: ignore
 from sympy import Integer  # type: ignore
