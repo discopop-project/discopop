783eacddb92920749da2a0ea165a0570aefff4f0
Lukas Rothenberger <lukas.rothenberger@tu-darmstadt.de>
2024-01-29 16:06:23 +0100

    feat(optimizer): use measured speedup for cost calculation

--- discopop_library/discopop_optimizer/suggestions/importers/do_all.py
+++ discopop_library/discopop_optimizer/suggestions/importers/do_all.py
@@ -150,10 +150,10 @@ def get_cost_multiplier(node_id: int, environment: Experiment, device_id: int) -
     Multiplier for Do-All:
         1 / Thread_count"""
     # get device specifications
+    # thread_count = environment.get_system().get_device(device_id).get_thread_count()
+    speedup = environment.get_system().get_device(device_id).get_measured_speedup()
+    multiplier = Integer(1) / speedup
 
-    thread_count = environment.get_system().get_device(device_id).get_thread_count()
-
-    multiplier = Integer(1) / thread_count
     cm = CostModel(multiplier, Integer(1))
 
     # return cm, [thread_count]
