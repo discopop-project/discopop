94a6a102e80b5c1bd6c9d5447d95ad7045d85d1e
Lukas Rothenberger <lukas.rothenberger@tu-darmstadt.de>
2024-02-15 16:13:49 +0100

    chore(scripts): cleanup console output

--- scripts/MPI_CC_wrapper.sh
+++ scripts/MPI_CC_wrapper.sh
@@ -35,9 +35,9 @@ LLVM_CLANGPP=$DP_BUILD_LLVM_BIN_DIR/clang++
 MPI_INCLUDES="$(mpicc -showme:compile)"
 
 # original arguments: "$@"
-echo "WRAPPED MPI CC COMPILE..."
-echo "ARGS: ${@}"
-echo "DP_FM_PATH: ${DP_FM_PATH}"
+#echo "WRAPPED MPI CC COMPILE..."
+#echo "ARGS: ${@}"
+#echo "DP_FM_PATH: ${DP_FM_PATH}"
 
 ## check if environment is prepared
 #if [ -z ${DP_FM_PATH} ]; then
@@ -47,7 +47,7 @@ echo "DP_FM_PATH: ${DP_FM_PATH}"
 #  exit 1
 #fi
 
-echo "${LLVM_CLANG} ${MPI_INCLUDES} -g -c  -S -emit-llvm -fno-discard-value-names ${@} -O0 -Xclang -load -Xclang ${DP_BUILD}/libi/LLVMDiscoPoP.so -DiscoPoP"
+#echo "${LLVM_CLANG} ${MPI_INCLUDES} -g -c  -S -emit-llvm -fno-discard-value-names ${@} -O0 -Xclang -load -Xclang ${DP_BUILD}/libi/LLVMDiscoPoP.so -DiscoPoP"
 #clang-11 -g -c -O0 -S -emit-llvm -fno-discard-value-names "$@" -Xclang -load -Xclang ${DP_BUILD}/libi/LLVMDiscoPoP.so -DiscoPoP -mllvm --fm-path -mllvm ${DP_FM_PATH}
 ${LLVM_CLANG} "${@}" -g -c -O0 -fno-discard-value-names -Xclang -load -Xclang ${DP_BUILD}/libi/LLVMDiscoPoP.so -DiscoPoP -fPIC ${MPI_INCLUDES} -Xlinker -lstdc++
 
