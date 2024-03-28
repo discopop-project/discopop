8abae7abc7c2c84d2862d47ae3b2c59f3ca98c6e
Lukas Rothenberger <lukas.rothenberger@tu-darmstadt.de>
2024-03-28 14:47:06 +0100

    feat: add unittest: global_vars/miniFE_CSRMatrix_proxy/global_array

---
+++ test/do_all/simple/global_vars/miniFE_CSRMatrix_proxy/global_arrays/src/code.cpp
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <vector>
+
+#define N 1000
+
+int rows[N];
+int row_offsets[N];
+
+struct CSRMatrix {
+    CSRMatrix()
+    {}
+
+    ~CSRMatrix()
+    {}
+
+    void prepare(unsigned nrows){
+        for(unsigned i = 0; i < nrows; ++i) {
+	        rows[i] = 0;
+	        row_offsets[i] = 0;
+        }
+    }
+};
+
+
+int main(int argc, const char* argv[]) {
+    int n = N;
+    CSRMatrix mat = CSRMatrix();
+    mat.prepare(n);
+return 0;
+}
