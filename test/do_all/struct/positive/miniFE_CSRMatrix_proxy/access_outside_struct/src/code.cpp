04b143e0295cfe37270bcf8cb2c9c815c2399096
Lukas Rothenberger <lukas.rothenberger@tu-darmstadt.de>
2024-03-28 14:22:35 +0100

    feat: add unittest: miniFE_CSRMatrix_proxy/access_outside_struct

---
+++ test/do_all/struct/positive/miniFE_CSRMatrix_proxy/access_outside_struct/src/code.cpp
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <vector>
+
+#define MINIFE_GLOBAL_ORDINAL int
+
+template<typename GlobalOrdinal>
+
+struct CSRMatrix {
+    CSRMatrix(): rows(), row_offsets()
+    {}
+
+    ~CSRMatrix()
+    {}
+
+    std::vector<GlobalOrdinal> rows;
+    std::vector<GlobalOrdinal> row_offsets;
+
+    void reserve_space(unsigned nrows){
+        rows.resize(nrows);
+        row_offsets.resize(nrows+1);
+    }
+};
+
+
+
+int main(int argc, const char* argv[]) {
+    int n = 1000;
+    CSRMatrix<MINIFE_GLOBAL_ORDINAL> mat = CSRMatrix<MINIFE_GLOBAL_ORDINAL>();
+
+    mat.reserve_space(n);
+    for(MINIFE_GLOBAL_ORDINAL i = 0; i < n; ++i) {
+	        mat.rows[i] = 0;
+	        mat.row_offsets[i] = 0;
+    }
+return 0;
+}
