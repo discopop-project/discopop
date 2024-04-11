8a0a3c8b0aa74e8205422fdd06114bfc574cb268
Lukas Rothenberger <lukas.rothenberger@tu-darmstadt.de>
2024-04-11 17:15:05 +0200

    feat: add unit tests

---
+++ test/optimizer/loop_collapse/negative/outer_loop_doall_only/src/code.cpp
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, const char* argv[]) {
+    static int n = 5000;
+    double *x = (double *) malloc(n * sizeof(double));
+    double *y = (double *) malloc(n * sizeof(double));
+    // Initialize x, y
+    // Do-All
+    for(int i = 0; i < n; ++i){
+        x[i] = 1.0;
+        y[i] = 2.0;
+    }
+
+    // Do-All
+    for(int i = 0; i < n/10; i++){
+        // Not Do-All
+        for(int j = 0; j < 100; j++){
+            y[i*10 + j] = x[i*10 + j] + y[i*10 +((j+2)%10)];
+        }
+    }
+}
