95df276009561c009e8b1cff848657010d9718a5
Lukas Rothenberger <lukas.rothenberger@tu-darmstadt.de>
2024-04-11 17:03:53 +0200

    fix: error in unit test

--- test/optimizer/loop_collapse/positive/simple_1/src/code.cpp
+++ test/optimizer/loop_collapse/positive/simple_1/src/code.cpp
@@ -6,14 +6,19 @@ int main(int argc, const char* argv[]) {
     double *x = (double *) malloc(n * sizeof(double));
     double *y = (double *) malloc(n * sizeof(double));
     // Initialize x, y
+    // Do-All
     for(int i = 0; i < n; ++i){
         x[i] = 1.0;
         y[i] = 2.0;
     }
 
+    // Do-All
     for(int i = 0; i < n/10; i++){
-        for(int j = 0; j < 11; j++){
-            y[i*j] = x[i*j] + y[i*j];
+        // Do-All
+        for(int j = 0; j < 10; j++){
+            y[i*10 + j] = x[i*10 + j] + y[i*10 +j];
         }
     }
+    free(x);
+    free(y);
 }
