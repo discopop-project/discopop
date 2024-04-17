4cf170989d68407d9c694adb91cde025557ba7ee
Lukas Rothenberger <lukas.rothenberger@tu-darmstadt.de>
2024-04-17 16:14:28 +0200

    fix: unit test

--- test/do_all/simple/nested/depth_2/positive/both_loops_doall/src/code.cpp
+++ test/do_all/simple/nested/depth_2/positive/both_loops_doall/src/code.cpp
@@ -16,7 +16,7 @@ int main(int argc, const char* argv[]) {
     for(int i = 0; i < n/10; i++){
         // Do-All
         for(int j = 0; j < 10; j++){
-            y[i*10 + j] = x[i*10 + j] + y[i*10 +j];
+            y[i*10 + j] = x[i*10 + j] / 42;
         }
     }
     free(x);
