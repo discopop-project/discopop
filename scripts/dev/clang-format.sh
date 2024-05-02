358f15fad4161648a4ae5e5128515ec2a7925a45
Lukas Rothenberger <lukas.rothenberger@tu-darmstadt.de>
2024-05-02 10:55:56 +0200

    feat[dev]: add clang-format script

---
+++ scripts/dev/clang-format.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# This file is part of the DiscoPoP software (http://www.discopop.tu-darmstadt.de)
+#
+# Copyright (c) 2020, Technische Universitaet Darmstadt, Germany
+#
+# This software may be modified and distributed under the terms of
+# the 3-Clause BSD License. See the LICENSE file in the package base
+# directory for details.
+
+# This script executes clang-format on all C/C++ files in the project
+
+# Usage: ./clang-format.sh
+
+# change directory to discopop
+cd ../..
+
+# loop over all C/C++ source and header files in the project
+C_CPP_FILES=$(find . -name *.c -or -name *.cpp -or -name *.h -or -name *.hpp)
+for file in $C_CPP_FILES; do
+    # execute clang-format for each file
+    clang-format $file -i
+done
