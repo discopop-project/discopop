125093db5a002838413182040b1369ae6749c8bc
Lukas Rothenberger <lukas.rothenberger@tu-darmstadt.de>
2024-05-06 11:24:47 +0200

    cleanup: remove unused code

--- rtlib/DPUtils.hpp
+++ rtlib/DPUtils.hpp
@@ -64,31 +64,6 @@ inline vector<string> *split(string input, char delim) {
   return substrings;
 }
 
-inline int32_t getFileID(string fileMapping, string fullPathName) {
-  int32_t index = 0; // if the associated file id is not found, then we return 0
-  string line;
-  ifstream fileMap(fileMapping.c_str());
-
-  if (fileMap.is_open()) {
-    vector<string> *substrings = NULL;
-    while (getline(fileMap, line)) {
-      substrings = split(line, '\t');
-      if (substrings->size() == 2) {
-        string indexString = (*substrings)[0];
-        string fileName = (*substrings)[1];
-        if (fileName.compare(fullPathName) == 0) {
-          index = (int32_t)atoi(indexString.c_str());
-          break;
-        }
-      }
-      substrings->clear();
-      delete substrings;
-    }
-    fileMap.close();
-  }
-  return index;
-}
-
 inline bool fexists(const string &filename) {
   ifstream ifile(filename.c_str());
 
