b2910fa0bda18a95c89ea5e50e1ae6d2594ca6b1
Lukas Rothenberger <lukas.rothenberger@tu-darmstadt.de>
2024-06-20 10:08:24 +0200

    feat: discopop_config_provider: add --full argument

--- discopop_library/ConfigProvider/config_provider.py
+++ discopop_library/ConfigProvider/config_provider.py
@@ -7,7 +7,7 @@
 # directory for details.
 
 from discopop_library.ConfigProvider.ConfigProviderArguments import ConfigProviderArguments
-from discopop_library.ConfigProvider.assets.build_config import DP_BUILD, DP_SOURCE, LLVM_BIN_DIR  # type: ignore
+from discopop_library.ConfigProvider.assets import build_config # DP_BUILD, DP_SOURCE, LLVM_BIN_DIR  # type: ignore
 from discopop_library.global_data.version.utils import get_version
 
 
@@ -15,11 +15,18 @@ def run(arguments: ConfigProviderArguments) -> str:
     """Returns the contents of the written build_config.txt"""
 
     if arguments.return_dp_build_dir:
-        return DP_BUILD  # type: ignore
+        return build_config.DP_BUILD  # type: ignore
     elif arguments.return_dp_source_dir:
-        return DP_SOURCE  # type: ignore
+        return build_config.DP_SOURCE  # type: ignore
     elif arguments.return_llvm_bin_dir:
-        return LLVM_BIN_DIR  # type: ignore
+        return build_config.LLVM_BIN_DIR  # type: ignore
+    elif arguments.return_full_config:
+        ret_str = ""
+        for name in [n for n in build_config.__dict__ if not n.startswith("_")]:
+            if len(ret_str) != 0:
+                ret_str += "\n"
+            ret_str += name +": " + build_config.__dict__[name]
+        return ret_str
     elif arguments.return_version_string:
         return get_version()
     else:
