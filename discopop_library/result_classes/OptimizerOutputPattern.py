08004ee5604d24a03a398b1a36bcf8379da06f7e
Lukas Rothenberger <lukas.rothenberger@tu-darmstadt.de>
2024-07-31 10:04:06 +0200

    refactor: PatternBase

--- discopop_library/result_classes/OptimizerOutputPattern.py
+++ discopop_library/result_classes/OptimizerOutputPattern.py
@@ -10,7 +10,7 @@
 import json
 from typing import Any, Dict, List
 from discopop_explorer.classes.Node import Node
-from discopop_explorer.pattern_detectors.PatternBase import PatternBase
+from discopop_explorer.classes.patterns.PatternBase import PatternBase
 
 from discopop_library.PatternIdManagement.unique_pattern_id import get_unique_pattern_id
 from discopop_library.discopop_optimizer.Variables.Experiment import Experiment
