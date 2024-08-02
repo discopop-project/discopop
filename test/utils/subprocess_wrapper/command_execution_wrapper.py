5a8e488fc0e0cdb576ae6a81d32b0ae1d324359a
Lukas Rothenberger <lukas.rothenberger@tu-darmstadt.de>
2024-08-02 09:29:46 +0200

    fix: revert: show test output
    This reverts commit e8ea956ed4495d45d4c07be046984cc920a44a1a.

--- test/utils/subprocess_wrapper/command_execution_wrapper.py
+++ test/utils/subprocess_wrapper/command_execution_wrapper.py
@@ -2,13 +2,13 @@ import subprocess
 from typing import Dict
 
 
-def run_cmd(cmd: str, cwd: str, env):
+def run_cmd(cmd: str, cwd: str, env: Dict[str, str]) -> None:
     subprocess.run(
         cmd,
         cwd=cwd,
         executable="/bin/bash",
         shell=True,
         env=env,
-        #        stdout=subprocess.DEVNULL,
-        #        stderr=subprocess.DEVNULL,
+        stdout=subprocess.DEVNULL,
+        stderr=subprocess.DEVNULL,
     )
