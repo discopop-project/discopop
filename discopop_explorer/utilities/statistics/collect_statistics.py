b1bc2c3d7cd8b34dee59943f9909db1a3bdedebe
Lukas Rothenberger <lukas.rothenberger@tu-darmstadt.de>
2024-08-28 15:57:52 +0200

    feat(statistics): maximum call path depth

--- discopop_explorer/utilities/statistics/collect_statistics.py
+++ discopop_explorer/utilities/statistics/collect_statistics.py
@@ -10,6 +10,8 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 import logging
 
+from discopop_explorer.utilities.statistics.maximum_call_path_depth import get_maximum_call_path_depth
+
 if TYPE_CHECKING:
     from discopop_explorer.discopop_explorer import ExplorerArguments
     from discopop_library.result_classes.DetectionResult import DetectionResult
@@ -19,4 +21,6 @@ logger = logging.getLogger("statistics")
 
 def collect_statistics(arguments: ExplorerArguments, res: DetectionResult) -> None:
     logger.info("Collecting code statistics...")
+    maximum_call_path_depth = get_maximum_call_path_depth(res.pet)
+    logger.debug("--> maximum_call_path_depth: " + str(maximum_call_path_depth))
     pass
