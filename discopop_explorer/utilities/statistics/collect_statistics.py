cec1da0ec805c3baf462bdfccf42cd25d052d06b
Lukas Rothenberger <lukas.rothenberger@tu-darmstadt.de>
2024-08-28 16:39:17 +0200

    feat(statistics): suggestion_num_function_calls

--- discopop_explorer/utilities/statistics/collect_statistics.py
+++ discopop_explorer/utilities/statistics/collect_statistics.py
@@ -11,6 +11,7 @@ from typing import TYPE_CHECKING
 import logging
 
 from discopop_explorer.utilities.statistics.maximum_call_path_depth import get_maximum_call_path_depth
+from discopop_explorer.utilities.statistics.num_function_calls import get_suggestion_num_function_calls
 from discopop_explorer.utilities.statistics.suggestion_call_path_depths import get_suggestion_call_path_depths
 
 if TYPE_CHECKING:
@@ -31,6 +32,16 @@ def collect_statistics(arguments: ExplorerArguments, res: DetectionResult) -> No
             [
                 res.pet.node_at(key).start_position() + " => " + str(suggestion_call_path_depths[key])
                 for key in suggestion_call_path_depths
+            ]
+        )
+    )
+    suggestion_num_function_calls = get_suggestion_num_function_calls(res)
+    logger.debug(
+        "--> suggestion_num_function_calls: "
+        + str(
+            [
+                res.pet.node_at(key).start_position() + " => " + str(suggestion_num_function_calls[key])
+                for key in suggestion_num_function_calls
             ]
         )
     )
---
+++ discopop_explorer/utilities/statistics/num_function_calls.py
@@ -0,0 +1,28 @@
+# This file is part of the DiscoPoP software (http://www.discopop.tu-darmstadt.de)
+#
+# Copyright (c) 2020, Technische Universitaet Darmstadt, Germany
+#
+# This software may be modified and distributed under the terms of
+# the 3-Clause BSD License.  See the LICENSE file in the package base
+# directory for details.
+
+
+from typing import Dict, Set
+from discopop_explorer.aliases.NodeID import NodeID
+from discopop_explorer.utilities.statistics.utilities.num_function_calls import get_num_function_calls
+from discopop_library.result_classes.DetectionResult import DetectionResult
+
+
+def get_suggestion_num_function_calls(res: DetectionResult) -> Dict[NodeID, int]:
+    res_dict: Dict[NodeID, int] = dict()
+
+    # collect NodeIDs where suggestions are located
+    node_ids: Set[NodeID] = set()
+    for pattern_type in res.patterns.__dict__:
+        for pattern in res.patterns.__dict__[pattern_type]:
+            node_ids.add(pattern.node_id)
+
+    # collect number of function calls in entire subtree of a parallelization suggestion
+    for node_id in node_ids:
+        res_dict[node_id] = get_num_function_calls(res.pet, res.pet.node_at(node_id), [])
+    return res_dict
