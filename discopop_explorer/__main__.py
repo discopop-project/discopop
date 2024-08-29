dd40a2bf3cc4538378b356d81f85e7420503b8bd
Lukas Rothenberger <lukas.rothenberger@tu-darmstadt.de>
2024-08-29 14:14:21 +0200

    feat(statistics): enable calculation of statistics by default

--- discopop_explorer/__main__.py
+++ discopop_explorer/__main__.py
@@ -105,7 +105,7 @@ def parse_args() -> ExplorerArguments:
         help="Path to llvm-cxxfilt executable. Required for task pattern detector if non-standard path should be used.",
     )
     experimental_parser.add_argument(
-        "--statistics", action="store_true", help="Calculate and store statistics for code and generated suggestions."
+        "--disable-statistics", action="store_false", help="Disable the calculation and storing of statistics for code and generated suggestions."
     )
     # fmt: on
 
@@ -159,7 +159,7 @@ def parse_args() -> ExplorerArguments:
         log_level=arguments.log.upper(),
         write_log=arguments.write_log,
         load_existing_doall_and_reduction_patterns=arguments.load_existing_doall_and_reduction_patterns,
-        collect_statistics=arguments.statistics,
+        collect_statistics=arguments.disable_statistics,
     )
 
 
