409c2aaf4b4b507072160b888e924863c58c044d
Lukas Rothenberger <lukas.rothenberger@tu-darmstadt.de>
2024-10-28 09:26:35 +0100

    feat(autotuner): add --sanitize argument (to invoke ThreadSanitizer)

--- discopop_library/EmpiricalAutotuning/Classes/ExecutionResult.py
+++ discopop_library/EmpiricalAutotuning/Classes/ExecutionResult.py
@@ -11,13 +11,23 @@ class ExecutionResult(object):
     runtime: float
     return_code: int
     result_valid: bool
+    thread_sanitizer: bool
 
-    def __init__(self, runtime: float, return_code: int, result_valid: bool):
+    def __init__(self, runtime: float, return_code: int, result_valid: bool, thread_sanitizer: bool):
         self.runtime = runtime
         self.return_code = return_code
         self.result_valid = result_valid
+        self.thread_sanitizer = thread_sanitizer
 
     def __str__(self) -> str:
         return (
-            "" + "time: " + str(self.runtime) + " code: " + str(self.return_code) + " valid: " + str(self.result_valid)
+            ""
+            + "time: "
+            + str(self.runtime)
+            + " code: "
+            + str(self.return_code)
+            + " valid: "
+            + str(self.result_valid)
+            + " TSAN: "
+            + str(self.thread_sanitizer)
         )
