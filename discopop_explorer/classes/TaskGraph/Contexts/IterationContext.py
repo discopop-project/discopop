d407690dcb421c9a7cd0afc27682c44f81539d22
Lukas Rothenberger <lukas.rothenberger@tu-darmstadt.de>
2025-08-07 17:02:29 +0200

    feat: assign loop contexts

---
+++ discopop_explorer/classes/TaskGraph/Contexts/IterationContext.py
@@ -0,0 +1,30 @@
+# This file is part of the DiscoPoP software (http://www.discopop.tu-darmstadt.de)
+#
+# Copyright (c) 2020, Technische Universitaet Darmstadt, Germany
+#
+# This software may be modified and distributed under the terms of
+# the 3-Clause BSD License.  See the LICENSE file in the package base
+# directory for details.
+
+from typing import List, Set
+from discopop_explorer.classes.TaskGraph.Aliases import PETNode, PETNodeID
+from discopop_explorer.classes.TaskGraph.Contexts.Context import Context
+
+
+class IterationContext(Context):
+    parent_context: Context
+    contained_pet_nodes: Set[PETNode]
+
+    def __init__(self, parent_context: Context):
+        self.parent_context = parent_context
+        self.contained_pet_nodes = set()
+        super().__init__()
+
+    def add_pet_node(self, pet_node: PETNode) -> None:
+        self.contained_pet_nodes.add(pet_node)
+
+    def get_plot_border_color(self) -> str:
+        return "b"
+
+    def get_plot_face_color(self) -> str:
+        return "green"
---
+++ discopop_explorer/classes/TaskGraph/Contexts/LoopParentContext.py
@@ -0,0 +1,20 @@
+# This file is part of the DiscoPoP software (http://www.discopop.tu-darmstadt.de)
+#
+# Copyright (c) 2020, Technische Universitaet Darmstadt, Germany
+#
+# This software may be modified and distributed under the terms of
+# the 3-Clause BSD License.  See the LICENSE file in the package base
+# directory for details.
+
+from discopop_explorer.classes.TaskGraph.Aliases import PETNodeID
+from discopop_explorer.classes.TaskGraph.Contexts.Context import Context
+
+
+class LoopParentContext(Context):
+    pass
+
+    def get_plot_border_color(self) -> str:
+        return "b"
+
+    def get_plot_face_color(self) -> str:
+        return "cyan"
