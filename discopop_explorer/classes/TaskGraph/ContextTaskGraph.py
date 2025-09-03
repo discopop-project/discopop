3dcff4118079fd5ddda628c36f084996add9be89
Lukas Rothenberger <lukas.rothenberger@tu-darmstadt.de>
2025-09-03 13:47:07 +0200

    feat: simplifying graph

--- discopop_explorer/classes/TaskGraph/ContextTaskGraph.py
+++ discopop_explorer/classes/TaskGraph/ContextTaskGraph.py
@@ -7,9 +7,10 @@
 # directory for details.
 
 import logging
-from typing import Optional
+from typing import Dict, List, Optional, Tuple
 from matplotlib import pyplot as plt
 import networkx as nx
+from networkx import Graph
 from tqdm import tqdm
 from discopop_explorer.classes.PEGraph.PEGraphX import PEGraphX
 from discopop_explorer.classes.TaskGraph.Contexts.Context import Context
@@ -17,6 +18,11 @@ from discopop_explorer.classes.TaskGraph.TGNode import TGNode
 from discopop_explorer.classes.TaskGraph.TaskGraph import TaskGraph
 
 logger = logging.getLogger("Explorer")
+
+
+class CombinedContext(Context):
+    def get_label(self) -> str:
+        return "CombinedCTX"
 
 
 class ContextTaskGraph(object):
@@ -34,7 +40,9 @@ class ContextTaskGraph(object):
         plt.ion()
         # start processing
         self.__construct_from_task_graph()
-        self.__coarsen_graph()
+        self.__print_graph_statistics("Pre simplification")
+        self.__simplify_graph()
+        self.__print_graph_statistics("Post simplification")
 
         print("Waiting for user to close the Window...")
         # plt.show(block=True)
@@ -51,9 +59,141 @@ class ContextTaskGraph(object):
             for sink_ctx, dep in ctx.outgoing_dependencies:
                 self.add_edge(sink_ctx, ctx)
 
-    def __coarsen_graph(self) -> None:
-        """increases the sizes of the contexts"""
-        logger.info("TODO: Coarsen graph")
+    def __simplify_graph(self) -> None:
+        """Replace triangles in the graph with a CombinedContext node. Loop until no further triangles exist."""
+        logger.info("Simplifying graph...")
+        triangles_replaced = True
+        while triangles_replaced:
+            logger.info("--> iterating...")
+            triangles_replaced = False
+            queue: List[Context] = list(self.graph.nodes())
+            while len(queue) > 0:
+                node = queue.pop()
+                predecessors = self.get_predecessors(node)
+                if len(predecessors) < 2:
+                    continue
+                # check all combinations of predecessors for triangles
+                triangle_nodes: Optional[Tuple[Context, Context]] = None
+                for pred_1 in predecessors:
+                    for pred_2 in predecessors:
+                        if pred_1 == pred_2:
+                            continue
+                        # triangle exists, if pred_1 is a predecessor of pred_2 or vice versa
+                        if pred_1 in self.get_predecessors(pred_2) or pred_2 in self.get_predecessors(pred_1):
+                            triangle_nodes = (pred_1, pred_2)
+                            break
+                if triangle_nodes is None:
+                    # did not find a triangle
+                    continue
+                # replace triangle with CombinedContext
+                combined_context_node = CombinedContext()
+                self.add_node(combined_context_node)
+                # register contained contexts
+                if isinstance(node, CombinedContext):
+                    combined_context_node.contained_contexts = combined_context_node.contained_contexts.union(
+                        node.contained_contexts
+                    )
+                else:
+                    combined_context_node.contained_contexts.add(node)
+
+                if isinstance(triangle_nodes[0], CombinedContext):
+                    combined_context_node.contained_contexts = combined_context_node.contained_contexts.union(
+                        triangle_nodes[0].contained_contexts
+                    )
+                else:
+                    combined_context_node.contained_contexts.add(triangle_nodes[0])
+
+                if isinstance(triangle_nodes[1], CombinedContext):
+                    combined_context_node.contained_contexts = combined_context_node.contained_contexts.union(
+                        triangle_nodes[1].contained_contexts
+                    )
+                else:
+                    combined_context_node.contained_contexts.add(triangle_nodes[1])
+
+                # check validity of the transformation. Do not allow the creation of bi-directional edges
+                # -> get predecessors and successors
+                raw_outside_predecessors = [
+                    n
+                    for n in self.get_predecessors(node)
+                    + self.get_predecessors(triangle_nodes[0])
+                    + self.get_predecessors(triangle_nodes[1])
+                ]
+                raw_outside_successors = [
+                    n
+                    for n in self.get_successors(node)
+                    + self.get_successors(triangle_nodes[0])
+                    + self.get_successors(triangle_nodes[1])
+                ]
+                # -> remove duplicates
+                raw_outside_predecessors = list(set(raw_outside_predecessors))
+                raw_outside_successors = list(set(raw_outside_successors))
+                # -> cleanup
+                outside_predecessors = [
+                    n
+                    for n in raw_outside_predecessors
+                    if not (n == node or n == triangle_nodes[0] or n == triangle_nodes[1])
+                ]
+                outside_successors = [
+                    n
+                    for n in raw_outside_successors
+                    if not (n == node or n == triangle_nodes[0] or n == triangle_nodes[1])
+                ]
+                # -> check if bi-directional edges would be created
+                skip_triangle = False
+                for pred in outside_predecessors:
+                    if pred in outside_successors:
+                        # bi-directional edge would be created! Ignore triangle.
+                        skip_triangle = True
+                        break
+                if skip_triangle:
+                    continue
+
+                # redirect incoming edges
+                for pred in outside_predecessors:
+                    self.add_edge(pred, combined_context_node)
+
+                # redirect outgoing edges
+                for succ in outside_successors:
+                    self.add_edge(combined_context_node, succ)
+
+                # remove triangle nodes from queue
+                if triangle_nodes[0] in queue:
+                    queue.remove(triangle_nodes[0])
+                if triangle_nodes[1] in queue:
+                    queue.remove(triangle_nodes[1])
+                # delete triangle nodes
+                self.graph.remove_node(node)
+                self.graph.remove_node(triangle_nodes[0])
+                self.graph.remove_node(triangle_nodes[1])
+                # allow one more iteration
+                triangles_replaced = True
+
+        logger.info("--> removing trivial nodes")
+        to_be_removed: List[Context] = []
+        for node in tqdm(self.graph.nodes):
+            if len(self.get_predecessors(node)) == 0 and len(self.get_successors(node)) == 0:
+                to_be_removed.append(node)
+        for node in to_be_removed:
+            self.graph.remove_node(node)
+
+    def __print_graph_statistics(self, label: str = "") -> None:
+        logger.info("####################")
+        logger.info("# Graph statistics: " + label)
+        logger.info("# Node count: " + str(len(self.graph.nodes)))
+        logger.info("# Edge count:  " + str(len(self.graph.edges)))
+        logger.info("####################")
+
+    def get_predecessors(self, node: Optional[Context]) -> List[Context]:
+        if node is None:
+            return []
+        predecessors = list(set([s for s, t in self.graph.in_edges(node)]))
+        return predecessors
+
+    def get_successors(self, node: Optional[Context]) -> List[Context]:
+        if node is None:
+            return []
+        successors = list(set([t for s, t in self.graph.out_edges(node)]))
+        return successors
 
     def plot(self) -> None:
         self.update_plot()
@@ -65,7 +205,7 @@ class ContextTaskGraph(object):
         plt.clf()
 
         logger.info("---> generating layout...")
-        positions = nx.nx_pydot.pydot_layout(self.graph, prog="dot")
+        positions = nx.nx_pydot.pydot_layout(self.graph, prog="sfdp")  # prog="dot")
         logger.info("--->    Done.")
 
         # draw regular nodes
