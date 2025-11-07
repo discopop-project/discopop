cc85abc9afd5f933ae94d6a1f6caeea83995f351
Lukas Rothenberger <lukas.rothenberger@tu-darmstadt.de>
2025-11-07 18:42:21 +0100

    feat: improved deb package

--- scripts/dev/create_package.sh
+++ scripts/dev/create_package.sh
@@ -6,6 +6,8 @@
 # the 3-Clause BSD License.  See the LICENSE file in the package base
 # directory for details.
 
+rm -rvf packages
+
 # create a temporary copy of the code to build the package
 rm -rf tmp_package_build_dir
 mkdir -p tmp_package_build_dir
