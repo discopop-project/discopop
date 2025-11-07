2425975eb0af072d6dd78c71c5ed5026afce84ce
Lukas Rothenberger <lukas.rothenberger@tu-darmstadt.de>
2025-11-07 01:11:59 +0100

    fix[profiler]: get varDefLine

--- DiscoPoP/utils/variables/defLine.cpp
+++ DiscoPoP/utils/variables/defLine.cpp
@@ -11,75 +11,97 @@
  */
 
 #include "../../DiscoPoP.hpp"
+#include <unordered_set>
+#include <queue>
 
 string DiscoPoP::determineVariableDefLine(Instruction *I) {
   string varDefLine{"LineNotFound"};
 
   bool isGlobal = false;
   string varName = determineVariableName_static(&*I, isGlobal, true, "");
-  // varName = refineVarName(varName);
   varName = (varName.find(".addr") == varName.npos) ? varName : varName.erase(varName.find(".addr"), 5);
-  // varName.erase(varName.find(".addr"), 5);
-  // size_t pos = varName.find(".addr");
-  // if (pos != varName.npos)
-  //     varName.erase(varName.find(".addr"), 5);
 
   string varType = determineVariableType(&*I);
+//  std::cout << "\tvarName: " << varName << "\n";
+//  std::cout << "\tvarType: " << varType << "\n";
 
-  if (programGlobalVariablesSet.count(varName)) {
-    varDefLine = "GlobalVar";
-    // Find definition line of global variables
-    GlobalVariable *globalVariable = I->getParent()->getParent()->getParent()->getGlobalVariable(StringRef(varName));
-    if (globalVariable) {
-      MDNode *metadata = globalVariable->getMetadata("dbg");
-      if (metadata) {
-        if (isa<DIGlobalVariableExpression>(metadata)) {
-          varDefLine =
-              to_string(fileID) + ":" +
-              to_string(cast<DIGlobalVariableExpression>(globalVariable->getMetadata("dbg"))->getVariable()->getLine());
-        }
+  // search in prior Instructions and Basic Blocks for Debug records for varName to get defLines
+  bool found_var = false;
+
+  BasicBlock* current_bb = I->getParent();
+  Instruction* current_inst = I;
+  std::unordered_set<BasicBlock*> visited;
+  std::unordered_set<BasicBlock*> added_to_queue;
+  std::queue<BasicBlock*> queue;
+  while(current_inst){
+    visited.insert(current_bb);
+
+    auto dbg_record_range = current_inst->getDbgRecordRange();
+    auto dbg_variable_range = filterDbgVars(dbg_record_range);
+    for(DbgVariableRecord &DVR : dbg_variable_range){
+      std::string IRVarName = DVR.getValue()->getName().str();
+      std::string SRCVarName = DVR.getVariable()->getName().str();
+//      std::cout << "\t\t-> DbgRecord: IRVarName: " << IRVarName << "\n";
+//      std::cout << "\t\t\t-> VarType: " << DVR.getVariable()->getType()->getName().str() << "\n";
+//      std::cout << "\t\t\t-> SRCVarName: " << SRCVarName << "\n";
+//      outs() << "\t\t\t-> ValueType: ";
+//      DVR.getValue()->getType()->print(outs());
+//      outs() << "\n";
+
+      std::string type_str;
+      llvm::raw_string_ostream rso(type_str);
+      DVR.getValue()->getType()->print(rso);
+
+      if((varName == SRCVarName) && (varType == rso.str())){
+        found_var = true;
+        varDefLine = to_string(fileID) + ":" + to_string(DVR.getDebugLoc().getLine());
+        break;
       }
     }
-  }
 
-  // Start from the beginning of a function and look for the variable
-  Function *F = I->getFunction();
-  for (Function::iterator FI = F->begin(), FE = F->end(); FI != FE; ++FI) {
-    BasicBlock &BB = *FI;
-    for (BasicBlock::iterator BI = BB.begin(), E = BB.end(); BI != E; ++BI) {
-      if (DbgDeclareInst *DI = dyn_cast<DbgDeclareInst>(BI)) {
-
-        if (auto *N = dyn_cast<MDNode>(DI->getVariable())) {
-          if (auto *DV = dyn_cast<DILocalVariable>(N)) {
-            if (varType.find("ARRAY") != string::npos || varType.find("STRUCT") != string::npos) {
-              if (DV->getName() == varName) {
-                varDefLine = to_string(fileID) + ":" + to_string(DV->getLine());
-                break;
-              }
-            } else {
-              string vn = "----";
-              bool isGlobal;
-              AllocaInst *AI = dyn_cast_or_null<AllocaInst>(DI->getAddress());
-              if (AI) {
-                for (User *U : AI->users()) {
-                  if (StoreInst *SI = dyn_cast<StoreInst>(U)) {
-                    vn = determineVariableName_static(&*SI, isGlobal, true, "");
-                    break;
-                  } else if (LoadInst *LI = dyn_cast<LoadInst>(U)) {
-                    vn = determineVariableName_static(&*LI, isGlobal, true, "");
-                    break;
-                  }
-                }
-                if (vn == varName || vn == varName + ".addr") {
-                  varDefLine = to_string(fileID) + ":" + to_string(DV->getLine());
-                  break;
-                }
-              }
-            }
+    if(found_var) {
+      break;
+    }
+    else {
+      // proceed to prior instruction / basic block
+      auto prior_inst = current_inst->getPrevNode();
+      if(prior_inst == nullptr){
+        // check if prior basic block exists
+        for(auto prior_bb : predecessors(current_bb)){
+          if(visited.count(prior_bb)){
+            continue;
           }
+          if(added_to_queue.count(prior_bb)){
+            continue;
+          }
+          queue.push(prior_bb);
+          added_to_queue.insert(prior_bb);
+        }
+
+        BasicBlock* selected_prior_bb = nullptr;
+        if(queue.size() > 0){
+          selected_prior_bb = queue.front();
+          queue.pop();
+          // get last instruction of prior BasicBlock as prior_inst
+          prior_inst = selected_prior_bb->getTerminator();  // &(prior_bb->back());
+        }
+        else{
+          // no prior, non-visited basic block exists
+          break;
         }
       }
+
+      current_inst = prior_inst;
+      if(current_inst != nullptr){
+        current_bb = current_inst->getParent();
+      }
+
     }
   }
+
+  if((varDefLine == "LineNotFound") && (programGlobalVariablesSet.count(varName))){
+    varDefLine = "GlobalVar";
+  }
+
   return varDefLine;
 }
