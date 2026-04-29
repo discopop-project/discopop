9dd1710dab9403ecddae90048593574d811d78b8
Lukas Rothenberger <lukas.rothenberger@tu-darmstadt.de>
2026-04-29 23:23:22 +0200

    chore: add discopop_cxx to python venv

---
+++ profiler/discopop_cxx/__main__.py
@@ -0,0 +1,23 @@
+# This file is part of the DiscoPoP software (http://www.discopop.tu-darmstadt.de)
+#
+# Copyright (c) 2020, Technische Universitaet Darmstadt, Germany
+#
+# This software may be modified and distributed under the terms of
+# the 3-Clause BSD License.  See the LICENSE file in the package base
+# directory for details.
+import subprocess
+import sys
+from pathlib import Path
+
+
+def main() -> int:
+    site_packages = Path(__file__).parent.parent
+    libs_dir = site_packages / "discopop-profiler.libs"
+    cxx_wrapper = libs_dir / "CXX_wrapper.sh"
+    cmd = [cxx_wrapper] + sys.argv[1:]
+    result = subprocess.run(cmd)
+    return result.returncode
+
+
+if __name__ == "__main__":
+    sys.exit(main())
