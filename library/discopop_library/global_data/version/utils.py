01090e43b3c16da6d661a01d3174d723087cd5ed
Lukas Rothenberger <lukas.rothenberger@tu-darmstadt.de>
2026-05-04 17:33:52 +0200

    ci: bump library version

--- library/discopop_library/global_data/version/utils.py
+++ library/discopop_library/global_data/version/utils.py
@@ -11,6 +11,7 @@ from pathlib import Path
 
 
 def get_version() -> str:
-    parent_folder_path = Path(__file__).parent.absolute()
-    with open(os.path.join(parent_folder_path, "VERSION")) as f:
-        return f.read().rstrip()
+    # parent_folder_path = Path(__file__).parent.absolute()
+    # with open(os.path.join(parent_folder_path, "VERSION")) as f:
+    #    return f.read().rstrip()
+    return "CURRENTLY INVALID!"
